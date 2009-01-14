cdbdcf5c312b263f7ee59db87554b376a49157cf
Gojko Adzic <gojko@gojko.com>
2009-01-14 21:20:51 +0000

    result set value accessor, insert & api of dbenvironment changed to work with this instead of supportsOutputOnInsert.

---
+++ dbfit-2.x/dbfit-core/src/main/java/dbfit/util/DbResultSetValueAccessor.java
@@ -0,0 +1,31 @@
+package dbfit.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbResultSetValueAccessor extends DbParameterAccessor {
+	
+	public DbResultSetValueAccessor (DbParameterAccessor c){
+		super(c.getName(),DbParameterAccessor.OUTPUT,c.getSqlType(),c.type,c.getPosition());
+	}
+	private PreparedStatement statement;
+	public void bindTo(PreparedStatement cs, int ind) throws SQLException{
+		this.statement=cs;
+	}
+	public void set(Object value) throws Exception {
+			throw new UnsupportedOperationException("Trying to set value of output parameter "+getName());
+	}	
+	public Object get() throws IllegalAccessException, InvocationTargetException {
+		try{
+		ResultSet rs=statement.getResultSet();
+		if (rs.next()) //todo: first try to find by name 
+			return rs.getObject(1);
+		}
+		catch (SQLException sqle){
+			throw new InvocationTargetException(sqle);
+		}
+		throw new IllegalAccessException("statement has not generated any keys");
+	}
+}
