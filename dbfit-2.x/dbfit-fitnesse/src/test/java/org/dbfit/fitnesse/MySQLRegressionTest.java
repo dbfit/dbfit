d50cf8b2813b4ed99169bf680c905f027419473f
Gojko Adzic <gojko@gojko.com>
2009-01-22 00:48:07 +0000

    integrated with latest trinidad snapshot

--- dbfit-2.x/dbfit-fitnesse/src/test/java/org/dbfit/fitnesse/MySQLRegressionTest.java
+++ dbfit-2.x/dbfit-fitnesse/src/test/java/org/dbfit/fitnesse/MySQLRegressionTest.java
@@ -5,19 +5,23 @@ import java.io.File;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.neuri.trinidad.fitnesserunner.JUnitHelper;
+import com.neuri.trinidad.JUnitHelper;
+import com.neuri.trinidad.fitnesserunner.FITTestEngine;
+import com.neuri.trinidad.fitnesserunner.FitNesseRepository;
 
 public class MySQLRegressionTest {
 	JUnitHelper helper;
+
 	@Before
 	public void initHelper() throws Exception{
-		helper=new JUnitHelper(new File(System.getProperty("java.io.tmpdir"),"dbfit-tests").getAbsolutePath(),	
-			"src/main/fitnesse");
-	}	
-	@Test
-	public void runFlowModeSuite() throws Exception{
-		helper.assertSuitePasses("AcceptanceTests.JavaTests.MySqlTests.FlowMode");
-//		helper.assertTestPasses("AcceptanceTests.JavaTests.MySqlTests.FlowMode.DataTypes.DateTests");
+		helper=new JUnitHelper(new FitNesseRepository("src/main/fitnesse"),
+			new FITTestEngine(),
+			new File(System.getProperty("java.io.tmpdir"),"dbfit-tests").getAbsolutePath());
+	}	@Test
+	public void runFlowModeSuite() throws Exception {
+		helper
+				.assertSuitePasses("AcceptanceTests.JavaTests.MySqlTests.FlowMode");
+		// helper.assertTestPasses("AcceptanceTests.JavaTests.MySqlTests.FlowMode.DataTypes.DateTests");
 	}
 
 }
