a0f936408de815f8f7123086390649b51f7d21aa
Pål Brattberg <brattberg@gmail.com>
2009-01-25 08:55:16 +0000

    Updated start-script to match new locations

--- dbfit-2.x/dbfit-fitnesse/src/test/resources/startFitnesse.bat
+++ dbfit-2.x/dbfit-fitnesse/src/test/resources/startFitnesse.bat
@@ -4,9 +4,11 @@ title Fitnesse on port %FITNESSE_PORT%
 REM set JAVA_HOME = location_of_java
 
 REM # For local running (without Maven installed)
-"%JAVA_HOME%\bin\java" -cp ..\..\target\fitnesse.jar fitnesse.FitNesse -p %FITNESSE_PORT% -e 0 -d .. -o %1 %2 %3 %4 %5 
-
+REM "%JAVA_HOME%\bin\java" -cp ..\..\target\fitnesse.jar fitnesse.FitNesse -p %FITNESSE_PORT% -e 0 -d .. -o %1 %2 %3 %4 %5 
 REM # When running from Maven-aware environments
-REM set FITNESSE_VERSION=20080812
-REM "%JAVA_HOME%\bin\java" -DFITNESSE_VERSION=%FITNESSE_VERSION% -DM2_REPO=%M2_REPO% -cp %M2_REPO%/org/fitnesse/fitnesse/%FITNESSE_VERSION%/fitnesse-%FITNESSE_VERSION%.jar fitnesse.FitNesse -p %FITNESSE_PORT% -e 0 -o %1 %2 %3 %4 %5 
+set M2_REPO="%USERPROFILE%\.m2\repository"
+set FITNESSE_VERSION=20081201
+set FITNESSE_JAR=%M2_REPO%/org/fitnesse/fitnesse/%FITNESSE_VERSION%/fitnesse-%FITNESSE_VERSION%.jar
+echo Using FitNesse JAR from %FITNESSE_JAR%
+"%JAVA_HOME%\bin\java" -DFITNESSE_VERSION=%FITNESSE_VERSION% -DM2_REPO=%M2_REPO% -cp %FITNESSE_JAR% fitnesse.FitNesse -p %FITNESSE_PORT% -e 0 -d ../../main/fitnesse -o %1 %2 %3 %4 %5 
 
