e7d7e1926a3a1cbc3766775a490c4e9e42c98640
Jake Benilov <benilov@gmail.com>
2012-12-30 20:24:44 +0000

    bumping FitNesse dependency & fixing mysql tests

---
+++ dbfit-java/mysql/src/test/java/dbfit/environment/MySQLRegressionTest.java
@@ -0,0 +1,28 @@
+package dbfit.environment;
+
+import fitnesse.junit.JUnitHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+public class MySQLRegressionTest {
+
+    private JUnitHelper helper;
+
+    @Before
+    public void setUp() {
+        helper = new JUnitHelper("../..", new File(System.getProperty("java.io.tmpdir"),"fitnesse").getAbsolutePath());
+        helper.setPort(1234);
+    }
+
+    @Test
+    public void flowMode() throws Exception {
+        helper.assertSuitePasses("AcceptanceTests.JavaTests.MySqlTests.FlowMode");
+    }
+
+    @Test
+    public void standaloneMode() throws Exception {
+        helper.assertSuitePasses("AcceptanceTests.JavaTests.MySqlTests.StandaloneFixtures");
+    }
+}
