4da4b661981210b4277ed218168d106e4ba82873
Jake Benilov <benilov@gmail.com>
2013-01-02 23:50:28 +0000

    adding sqlserver module to root project
    the regression tests are ignored for now

---
+++ dbfit-java/sqlserver/src/test/java/dbfit/environment/SqlServerRegressionTest.java
@@ -0,0 +1,30 @@
+package dbfit.environment;
+
+import fitnesse.junit.JUnitHelper;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+
+@Ignore
+public class SqlServerRegressionTest {
+
+    private JUnitHelper helper;
+
+    @Before
+    public void setUp() {
+        helper = new JUnitHelper("../..", new File(System.getProperty("java.io.tmpdir"),"fitnesse").getAbsolutePath());
+        helper.setPort(1234);
+    }
+
+    @Test
+    public void flowMode() throws Exception {
+        helper.assertSuitePasses("AcceptanceTests.JavaTests.SqlServerTests.FlowMode");
+    }
+
+    @Test
+    public void standaloneMode() throws Exception {
+        helper.assertSuitePasses("AcceptanceTests.JavaTests.SqlServerTests.StandaloneFixtures");
+    }
+}
