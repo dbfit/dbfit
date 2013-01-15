a126c5b7f334b0665d9a5a124e172bd62f4e0a0a
Jake Benilov <benilov@gmail.com>
2013-01-15 09:03:41 +0000

    adding executables for complete zip

---
+++ bin/startFitnesse.bat
@@ -0,0 +1,2 @@
+java -cp lib\fitnesse-complete.jar fitnesse.FitNesse -p 8085 -e 0 -o %1 %2 %3 %4 %5 
+pause
---
+++ bin/startFitnesse.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+java -cp lib/fitnesse-complete.jar fitnesse.FitNesse -p 8085 -e 0 -o $1 $2 $3 $4 $5
+
+
