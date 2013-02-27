b7667cd9f04bc8bcaa1ec3433a5dd88176b43a17
Jake Benilov <benilov@gmail.com>
2013-02-27 19:24:56 +0000

    reformatting ExecuteProcedure.java (#19)

--- dbfit-java/core/src/main/java/dbfit/fixture/ExecuteProcedure.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/ExecuteProcedure.java
@@ -1,54 +1,59 @@
 package dbfit.fixture;
 
-import java.sql.SQLException;
-
-
 import dbfit.api.DBEnvironment;
 import dbfit.api.DbEnvironmentFactory;
 import dbfit.api.DbObject;
 import dbfit.api.DbStoredProcedure;
 import dbfit.util.ExpectedBehaviour;
 
-public class ExecuteProcedure extends DbObjectExecutionFixture{
-	private DBEnvironment environment;
-	private String procName;
-	private boolean exceptionExpected=false;
-    private boolean excNumberDefined=false;
+import java.sql.SQLException;
+
+public class ExecuteProcedure extends DbObjectExecutionFixture {
+    private DBEnvironment environment;
+    private String procName;
+    private boolean exceptionExpected = false;
+    private boolean excNumberDefined = false;
     private int excNumberExpected;
-	public ExecuteProcedure()
-    {
+
+    public ExecuteProcedure() {
         this.environment = DbEnvironmentFactory.getDefaultEnvironment();
     }
-	public ExecuteProcedure(DBEnvironment dbEnvironment, String procName, 
-			int expectedErrorCode) {
-		this.procName= procName;
-		this.environment = dbEnvironment;		
-		this.exceptionExpected=true;
-		this.excNumberDefined=true;
-		this.excNumberExpected=expectedErrorCode;
-	}
-	public ExecuteProcedure(DBEnvironment dbEnvironment, String procName, 
-			boolean exceptionExpected) {
-		this.procName= procName;
-		this.environment = dbEnvironment;		
-		this.exceptionExpected=exceptionExpected;
-		this.excNumberDefined=false;
-	}
-	public ExecuteProcedure(DBEnvironment dbEnvironment, String procName) {
-		this(dbEnvironment,procName,false);		
-	}
-	@Override
-	protected DbObject getTargetDbObject() throws SQLException {
-		return new DbStoredProcedure(environment,procName);
-	}
-	@Override
-	protected ExpectedBehaviour getExpectedBehaviour() {
-		if (!exceptionExpected) return ExpectedBehaviour.NO_EXCEPTION;
-		if (!excNumberDefined) return ExpectedBehaviour.ANY_EXCEPTION;
-		return ExpectedBehaviour.SPECIFIC_EXCEPTION;
-	}
-	@Override
-	protected int getExpectedErrorCode() {
-		return excNumberExpected;
-	}
+
+    public ExecuteProcedure(DBEnvironment dbEnvironment, String procName,
+                            int expectedErrorCode) {
+        this.procName = procName;
+        this.environment = dbEnvironment;
+        this.exceptionExpected = true;
+        this.excNumberDefined = true;
+        this.excNumberExpected = expectedErrorCode;
+    }
+
+    public ExecuteProcedure(DBEnvironment dbEnvironment, String procName,
+                            boolean exceptionExpected) {
+        this.procName = procName;
+        this.environment = dbEnvironment;
+        this.exceptionExpected = exceptionExpected;
+        this.excNumberDefined = false;
+    }
+
+    public ExecuteProcedure(DBEnvironment dbEnvironment, String procName) {
+        this(dbEnvironment, procName, false);
+    }
+
+    @Override
+    protected DbObject getTargetDbObject() throws SQLException {
+        return new DbStoredProcedure(environment, procName);
+    }
+
+    @Override
+    protected ExpectedBehaviour getExpectedBehaviour() {
+        if (!exceptionExpected) return ExpectedBehaviour.NO_EXCEPTION;
+        if (!excNumberDefined) return ExpectedBehaviour.ANY_EXCEPTION;
+        return ExpectedBehaviour.SPECIFIC_EXCEPTION;
+    }
+
+    @Override
+    protected int getExpectedErrorCode() {
+        return excNumberExpected;
+    }
 }
