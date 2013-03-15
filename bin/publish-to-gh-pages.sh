5c41dde467db781db8808e4aef11f86eb8cc20e6
Jake Benilov <benilov@gmail.com>
2013-03-15 16:37:16 +0000

    publish-to-gh-pages now pushes gh-pages branch

--- bin/publish-to-gh-pages.sh
+++ bin/publish-to-gh-pages.sh
@@ -4,3 +4,4 @@ doc_sha=$(git ls-tree -d HEAD website | awk '{print $3}')
 doc_commit_message=$(git log -n 1 --format="%s" $doc_dir)
 new_commit=$(echo $doc_commit_message | git commit-tree $doc_sha -p $parent_sha)
 git update-ref refs/heads/gh-pages $new_commit
+git push origin gh-pages
