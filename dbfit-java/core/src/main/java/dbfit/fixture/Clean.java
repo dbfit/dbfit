6964e4019a4bb957a6e921cd9fbeecd8904e175f
Jake Benilov <benilov@gmail.com>
2013-03-17 11:45:41 +0000

    separated fit from db code in Clean (#51)

--- dbfit-java/core/src/main/java/dbfit/fixture/Clean.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/Clean.java
@@ -7,16 +7,18 @@ import fit.Parse;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Clean extends fit.ColumnFixture {
-    private DBEnvironment environment;
+    private DeleteData deleteData;
 
     public Clean(DBEnvironment environment) {
-        this.environment = environment;
+        this.deleteData = new DeleteData(environment);
     }
 
     public Clean() {
-        this.environment = DbEnvironmentFactory.getDefaultEnvironment();
+        this.deleteData = new DeleteData(DbEnvironmentFactory.getDefaultEnvironment());
     }
 
     public String table;
@@ -25,50 +27,21 @@ public class Clean extends fit.ColumnFixture {
     public String[] keys;
     public String where = null;
 
-    private String getIDCSV() {
-        StringBuilder sb = new StringBuilder();
-        String comma = "";
-        for (BigDecimal x : ids) {
-            sb.append(comma);
-            sb.append(x.toString());
-            comma = ", ";
-        }
-        return sb.toString();
-    }
-
-    private String getKeyCSV() {
-        StringBuilder sb = new StringBuilder();
-        String comma = "";
-        for (String x : keys) {
-            sb.append(comma);
-            sb.append("'");
-            sb.append(x.toString());
-            sb.append("'");
-            comma = ", ";
-        }
-        return sb.toString();
-    }
-
     private boolean hadRowOperation = false;
 
     public boolean clean() throws SQLException {
-        String s = "Delete from " + table + (where != null ? (" where " + where) : "");
-        environment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), s).execute();
+        deleteData.deleteTable(table, where);
         return true;
     }
 
     public boolean DeleteRowsForIDs() throws SQLException {
-        String s = "Delete from " + table + " where " + columnName + " in ("
-                + getIDCSV() + ") " + (where != null ? " and " + where : "");
-        environment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), s).execute();
+        deleteData.deleteTableByIds(table, where, columnName, ids);
         hadRowOperation = true;
         return true;
     }
 
     public boolean DeleteRowsForKeys() throws SQLException {
-        String s = "Delete from " + table + " where " + columnName + " in ("
-                + getKeyCSV() + ") " + (where != null ? " and " + where : "");
-        environment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), s).execute();
+        deleteData.deleteTableByKeys(table, where, columnName, keys);
         hadRowOperation = true;
         return true;
     }
@@ -82,6 +55,90 @@ public class Clean extends fit.ColumnFixture {
             } catch (SQLException sqle) {
                 exception(row, sqle);
             }
+        }
+    }
+
+    public static class DeleteData {
+        private DBEnvironment environment;
+
+        public DeleteData(DBEnvironment environment) {
+            this.environment = environment;
+        }
+
+        public void deleteTable(String tableName, String whereClause) throws SQLException {
+            SQLQuery q = new DeleteQuery(tableName, whereClause);
+            executeQuery(q);
+        }
+
+        public void deleteTableByKeys(String tableName, String whereClause, String columnName, Object[] keys) throws SQLException {
+            SQLQuery q = new DeleteByInclusionQuery(tableName, whereClause, columnName, keys, true);
+            executeQuery(q);
+        }
+        
+        public void deleteTableByIds(String tableName, String whereClause, String columnName, Object[] ids) throws SQLException {
+            SQLQuery q = new DeleteByInclusionQuery(tableName, whereClause, columnName, ids, false);
+            executeQuery(q);
+        }
+
+        private void executeQuery(SQLQuery q) throws SQLException {
+            environment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), q.toString()).execute();
+        }
+    }
+
+    public static class SQLQuery {
+        private String statement;
+        protected List<String> whereClauses = new ArrayList<String>();
+
+        public SQLQuery(String statement, String... whereClauses) {
+            this.statement = statement;
+            for (String whereClause : whereClauses) {
+                if (whereClause != null) {
+                    this.whereClauses.add(whereClause);
+                }
+            }
+        }
+
+        protected static String whereColumnIn(String columnName, Object[] values, boolean quoted) {
+            return columnName + " in (" + joinedWithCommas(values, quoted) + ")";
+        }
+
+        protected static String joinedWithCommas(Object[] ids, boolean quoted) {
+            StringBuilder sb = new StringBuilder();
+            String comma = "";
+            for (Object x : ids) {
+                sb.append(comma);
+                if (quoted) sb.append("'");
+                sb.append(x.toString());
+                if (quoted) sb.append("'");
+                comma = ", ";
+            }
+            return sb.toString();
+        }
+
+        public String toString() {
+            StringBuilder s = new StringBuilder();
+            s.append(statement);
+            if (!whereClauses.isEmpty()) {
+                s.append(" where ");
+                s.append(whereClauses.get(0));
+                for (String whereClause : whereClauses.subList(1, whereClauses.size())) {
+                    s.append(" and ");
+                    s.append(whereClause);
+                }
+            }
+            return s.toString();
+        }
+    }
+
+    public static class DeleteQuery extends SQLQuery {
+        public DeleteQuery(String tableName, String... whereClauses) {
+            super("delete from " + tableName, whereClauses);
+        }
+    }
+
+    public static class DeleteByInclusionQuery extends DeleteQuery {
+        public DeleteByInclusionQuery(String tableName, String whereClause, String column, Object[] values, boolean quoted) {
+            super(tableName, whereClause, whereColumnIn(column, values, quoted));
         }
     }
 }
