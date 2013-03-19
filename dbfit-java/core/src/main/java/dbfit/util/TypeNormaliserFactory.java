c5bb1e4c33e5fbcbabc19e1638e1245beede7fb4
Yavor Nikolov <nikolov.javor@gmail.com>
2013-03-19 02:04:51 +0200

    TypeNormaliserFactory indentation reformatting (tabs to spaces)

--- dbfit-java/core/src/main/java/dbfit/util/TypeNormaliserFactory.java
+++ dbfit-java/core/src/main/java/dbfit/util/TypeNormaliserFactory.java
@@ -5,35 +5,35 @@ import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class TypeNormaliserFactory {
-	//private static Map<Class, TypeNormaliser> normalisers = new HashMap<Class, TypeNormaliser>();
-	private static Map<String, TypeNormaliser> normalisers = new HashMap<String, TypeNormaliser>();
+    //private static Map<Class, TypeNormaliser> normalisers = new HashMap<Class, TypeNormaliser>();
+    private static Map<String, TypeNormaliser> normalisers = new HashMap<String, TypeNormaliser>();
 
-	public static void setNormaliser(Class targetClass, TypeNormaliser normaliser) {
-		normalisers.put(targetClass.getCanonicalName(), normaliser);
-	}
+    public static void setNormaliser(Class targetClass, TypeNormaliser normaliser) {
+        normalisers.put(targetClass.getCanonicalName(), normaliser);
+    }
 
-	public static void setNormaliser(String targetClassName, TypeNormaliser normaliser) {
-		normalisers.put(targetClassName, normaliser);
-	}
+    public static void setNormaliser(String targetClassName, TypeNormaliser normaliser) {
+        normalisers.put(targetClassName, normaliser);
+    }
 
-	public static TypeNormaliser getNormaliser(Class targetClass) {
-		TypeNormaliser normaliser = normalisers.get(targetClass.getCanonicalName());
+    public static TypeNormaliser getNormaliser(Class targetClass) {
+        TypeNormaliser normaliser = normalisers.get(targetClass.getCanonicalName());
 
-		/*
-		if (normaliser == null) {
-			Class bestCandidate = targetClass;
-			for (Class c: normalisers.keySet()) {
-				if (bestCandidate.isAssignableFrom(c)) {
-					bestCandidate = c;
-				}
-			}
+        /*
+        if (normaliser == null) {
+            Class bestCandidate = targetClass;
+            for (Class c: normalisers.keySet()) {
+                if (bestCandidate.isAssignableFrom(c)) {
+                    bestCandidate = c;
+                }
+            }
 
-			if (bestCandidate != targetClass) {
-				normaliser = normalisers.get(bestCandidate);
-			}
-		}
-		*/
+            if (bestCandidate != targetClass) {
+                normaliser = normalisers.get(bestCandidate);
+            }
+        }
+        */
 
-		return normaliser;
-	}
+        return normaliser;
+    }
 }
