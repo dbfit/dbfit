c05fe25850c9a35f8825f7130d9331a768e5f8bb
Yavor Nikolov <nikolov.javor@gmail.com>
2013-03-23 13:51:17 +0200

    Bumped up Oracle JDBC driver build requirement to 11gR2 11.2.0.3

--- dbfit-java/oracle/redeploy-odjbc.bat
+++ dbfit-java/oracle/redeploy-odjbc.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=ojdbc14.jar -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.2.0 -Dpackaging=jar
+mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3.0 -Dpackaging=jar
