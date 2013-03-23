dcd3243fb73de36f4474c99d4006884b05c86b87
Jake Benilov <benilov@gmail.com>
2013-03-23 12:10:28 +0000

    better name for string in Query class(#cleanup)

--- dbfit-java/core/src/main/java/dbfit/fixture/Query.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/Query.java
@@ -12,7 +12,7 @@ import static dbfit.util.SymbolUtil.isSymbolGetter;
 
 public class Query extends RowSetFixture {
 	private DBEnvironment dbEnvironment;
-	private String query;
+	private String queryOrSymbol;
 	private boolean isOrdered;
 
 	public Query() {
@@ -20,28 +20,28 @@ public class Query extends RowSetFixture {
 		isOrdered = false;
 	}
 
-	public Query(DBEnvironment environment, String query) {
-		this(environment, query, false);
+	public Query(DBEnvironment environment, String queryOrSymbol) {
+		this(environment, queryOrSymbol, false);
 	}
 
-	public Query(DBEnvironment environment, String query, boolean isOrdered) {
+	public Query(DBEnvironment environment, String queryOrSymbol, boolean isOrdered) {
 		this.dbEnvironment = environment;
-		this.query = query;
+		this.queryOrSymbol = queryOrSymbol;
 		this.isOrdered = isOrdered;
 	}
 
 	public DataTable getDataTable() throws SQLException {
-		if (query == null)
-			query = args[0];
-		if (isSymbolGetter(query))
+		if (queryOrSymbol == null)
+			queryOrSymbol = args[0];
+		if (isSymbolGetter(queryOrSymbol))
 			return getFromSymbol();
-		Log.log("Query: '%s'", query);
-		PreparedStatement st = dbEnvironment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(),query);
+		Log.log("Query: '%s'", queryOrSymbol);
+		PreparedStatement st = dbEnvironment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), queryOrSymbol);
 		return new DataTable(st.executeQuery());
 	}
 
 	private DataTable getFromSymbol() throws SQLException {
-		Object o = dbfit.util.SymbolUtil.getSymbol(query);
+		Object o = dbfit.util.SymbolUtil.getSymbol(queryOrSymbol);
 		if (o instanceof ResultSet) {
 			return new DataTable((ResultSet) o);
 		} else if (o instanceof DataTable) {
