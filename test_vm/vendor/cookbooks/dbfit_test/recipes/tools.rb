b15df61e775a1ebf14b014ae7d352233849e828d
Christian Kraemer <spam@crasu.de>
2013-03-28 12:10:43 +0100

    removed rake as we can now use gradle for everything removed zip as the zip file is now created by gradle internally

--- test_vm/vendor/cookbooks/dbfit_test/recipes/tools.rb
+++ test_vm/vendor/cookbooks/dbfit_test/recipes/tools.rb
@@ -1,12 +1,5 @@
 include_recipe "maven"
 include_recipe "gradle"
-yum_package "rubygem-rake" do
-    action :install
-end
-
-yum_package "zip" do
-    action :install
-end
 
 package "git-core" do
   action :install
