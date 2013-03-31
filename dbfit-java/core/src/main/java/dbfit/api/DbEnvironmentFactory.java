3bf8d7fa676b986b420ef9bc4a7e3d568d4373c2
Yavor Nikolov <nikolov.javor@gmail.com>
2013-03-31 12:25:20 +0300

    DbEnvironmentFactory reformatting indentation

--- dbfit-java/core/src/main/java/dbfit/api/DbEnvironmentFactory.java
+++ dbfit-java/core/src/main/java/dbfit/api/DbEnvironmentFactory.java
@@ -3,11 +3,13 @@ package dbfit.api;
 import dbfit.api.DBEnvironment;
 
 public class DbEnvironmentFactory {
-	private static DBEnvironment environment;
-	public static DBEnvironment getDefaultEnvironment(){
-		return environment;
-	}
-	public static void setDefaultEnvironment( DBEnvironment newDefaultEnvironment){
-		environment=newDefaultEnvironment;
-	}
+    private static DBEnvironment environment;
+    public static DBEnvironment getDefaultEnvironment(){
+        return environment;
+    }
+
+    public static void setDefaultEnvironment(DBEnvironment newDefaultEnvironment){
+        environment=newDefaultEnvironment;
+    }
 }
+
