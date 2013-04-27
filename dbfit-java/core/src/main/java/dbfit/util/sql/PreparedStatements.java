82ab2c7f415df62744835a1da7e697c4710a52be
Jake Benilov <benilov@gmail.com>
2013-04-27 19:59:36 +0100

    remove another superfluous comment

--- dbfit-java/core/src/main/java/dbfit/util/sql/PreparedStatements.java
+++ dbfit-java/core/src/main/java/dbfit/util/sql/PreparedStatements.java
@@ -3,13 +3,6 @@ package dbfit.util.sql;
 import static dbfit.util.LangUtils.join;
 import static dbfit.util.LangUtils.repeat;
 
-/**
- * Created with IntelliJ IDEA.
- * User: benilovj
- * Date: 4/25/13
- * Time: 8:31 PM
- * To change this template use File | Settings | File Templates.
- */
 public class PreparedStatements {
     public static String buildStoredProcedureCall(String procName, int numberOfInputParameters) {
         String inputs = join(repeat("?", numberOfInputParameters), ",");
