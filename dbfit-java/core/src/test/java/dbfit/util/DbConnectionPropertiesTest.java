a7137009fd3bed76f8ceaa9a642d8956f7e00268
Yavor Nikolov <nikolov.javor@gmail.com>
2013-04-27 17:15:58 +0300

    Add a mock test for DbConnectionProperties -> CryptoService interaction

--- dbfit-java/core/src/test/java/dbfit/util/DbConnectionPropertiesTest.java
+++ dbfit-java/core/src/test/java/dbfit/util/DbConnectionPropertiesTest.java
@@ -5,20 +5,32 @@ import dbfit.util.crypto.CryptoServiceFactory;
 
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.After;
+import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
+
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.*;
 
 import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DbConnectionPropertiesTest {
 
     private static final String DB_PASSWORD = "Test Password";
 
     private CryptoService cryptoService;
-
+    @Mock private CryptoService mockedCryptoService;
 
     @Before
     public void prepare() {
         cryptoService = CryptoServiceFactory.getCryptoService();
+    }
+
+    @After
+    public void cleanup() {
+        CryptoServiceFactory.setCryptoService(null);
     }
 
     private String getOriginalPassword() {
@@ -40,15 +52,27 @@ public class DbConnectionPropertiesTest {
         return lines;
     }
 
+    private List<String> prepareEncryptedSettings() {
+        return prepareEncryptedSettings(getOriginalPassword());
+    }
+
+    @Test
+    public void shouldCallDecryptWhenLoadingEncryptedPassword() {
+        List<String> lines = prepareEncryptedSettings();
+        CryptoServiceFactory.setCryptoService(mockedCryptoService);
+
+        DbConnectionProperties.CreateFromString(lines);
+        verify(mockedCryptoService, times(1)).decrypt(getOriginalPassword());
+    }
+
     @Test
     public void decryptedPasswordShouldMatchOriginalOne() {
-        String originalPassword = getOriginalPassword();
-        List<String> lines = prepareEncryptedSettings(originalPassword);
+        List<String> lines = prepareEncryptedSettings();
 
         DbConnectionProperties connProps =
             DbConnectionProperties.CreateFromString(lines);
 
-        assertEquals(originalPassword, connProps.Password);
+        assertEquals(getOriginalPassword(), connProps.Password);
     }
 }
 
