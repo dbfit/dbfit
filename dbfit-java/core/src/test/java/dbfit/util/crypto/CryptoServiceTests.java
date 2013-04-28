0ba078da578ee8a2cb44fb6b568a67e5eb06b9ba
Yavor Nikolov <nikolov.javor@gmail.com>
2013-04-28 15:40:44 +0300

    Refactored CryptoServiceTests (delegating to CryptoKeyStoreManager)

--- dbfit-java/core/src/test/java/dbfit/util/crypto/CryptoServiceTests.java
+++ dbfit-java/core/src/test/java/dbfit/util/crypto/CryptoServiceTests.java
@@ -1,14 +1,7 @@
 package dbfit.util.crypto;
 
-import static dbfit.util.crypto.JKSCryptoKeyService.KEY_ALIAS;
-import static dbfit.util.crypto.JKSCryptoKeyService.KS_TYPE;
-import static dbfit.util.crypto.JKSCryptoKeyService.KS_NAME;
-
-import javax.crypto.KeyGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.Key;
-import javax.crypto.SecretKey;
-import java.security.KeyStore;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -17,14 +10,17 @@ import static org.hamcrest.core.IsNot.not;
 import static org.junit.matchers.JUnitMatchers.*;
 
 import java.io.File;
-import java.io.FileOutputStream;
 
 public class CryptoServiceTests {
 
     public static final char[] TEST_KS_PASS = "dbfit-demo-pass".toCharArray();
 
+    private static CryptoKeyStoreManager getKsManager(File ksPath) {
+        return new CryptoKeyStoreManager(ksPath, TEST_KS_PASS);
+    }
+
     public static void initTestCryptoKeyStore(File ksPath) throws Exception {
-        createKeyStore(ksPath);
+        getKsManager(ksPath).createKeyStore();
         CryptoKeyServiceFactory.setKeyStoreLocation(ksPath, TEST_KS_PASS);
         CryptoKeyServiceFactory.setKeyService(null);
     }
@@ -34,32 +30,8 @@ public class CryptoServiceTests {
         CryptoKeyServiceFactory.setKeyService(null);
     }
 
-    private static void createKeyStore(File path) throws Exception {
-        KeyStore ks = KeyStore.getInstance(KS_TYPE);
-        ks.load(null, TEST_KS_PASS);
-        SecretKey mySecretKey = (SecretKey) generateKey();
-
-        FileOutputStream fos = null;
-
-        KeyStore.SecretKeyEntry skEntry = new KeyStore.SecretKeyEntry(mySecretKey);
-        ks.setEntry(KEY_ALIAS, skEntry, new KeyStore.PasswordProtection(
-                                                    TEST_KS_PASS));
-
-        try {
-            File ksFile = new File(path, KS_NAME);
-            fos = new java.io.FileOutputStream(ksFile);
-            ks.store(fos, TEST_KS_PASS);
-        } finally {
-            if (fos != null) {
-                fos.close();
-            }
-        }
- 
-    }
-
     public static Key generateKey() throws NoSuchAlgorithmException {
-        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        return kgen.generateKey();
+        return CryptoKeyStoreManager.generateKey();
     }
 
     public static void encryptedPasswordShouldNotContainOriginalOne(
