45f7c79c05bfd42288ba92c04e6ce07e39ba89a6
Yavor Nikolov <nikolov.javor@gmail.com>
2013-04-28 15:31:30 +0300

    CryptoKeyStoreManager added

---
+++ dbfit-java/core/src/main/java/dbfit/util/crypto/CryptoKeyStoreManager.java
@@ -0,0 +1,87 @@
+package dbfit.util.crypto;
+
+import javax.crypto.KeyGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.Key;
+import javax.crypto.SecretKey;
+import java.security.KeyStore;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class CryptoKeyStoreManager {
+    public static final String KS_TYPE = "JCEKS";
+    public static final String KS_NAME = ".dbfit.jks";
+    public static final char[] KS_PASS = "DbFit Access Key".toCharArray();
+    public static final String KEY_ALIAS = "dbfit";
+
+    private File keyStoreLocation;
+    private char[] keyStorePassword;
+
+    public static CryptoKeyStoreManager getDefaultInstance() {
+        return new CryptoKeyStoreManager();
+    }
+
+    public CryptoKeyStoreManager(File keyStorePath, char[] ksPassword) {
+        this.keyStoreLocation = keyStorePath;
+        this.keyStorePassword = ksPassword;
+    }
+
+    public CryptoKeyStoreManager(File keyStorePath) {
+        this(keyStorePath, KS_PASS);
+    }
+
+    public CryptoKeyStoreManager() {
+        this(getDefaultKeyStoreLocation());
+    }
+
+    public File getKeyStoreLocation() {
+        return keyStoreLocation;
+    }
+
+    public char[] getKeyStorePassword() {
+        return keyStorePassword;
+    }
+
+    public File getKeyStoreFile() {
+        return new File(getKeyStoreLocation(), KS_NAME);
+    }
+
+    public void createKeyStore() throws Exception {
+        KeyStore ks = KeyStore.getInstance(KS_TYPE);
+        ks.load(null, getKeyStorePassword());
+        SecretKey mySecretKey = (SecretKey) generateKey();
+
+
+        KeyStore.SecretKeyEntry skEntry = new KeyStore.SecretKeyEntry(mySecretKey);
+        ks.setEntry(KEY_ALIAS, skEntry, new KeyStore.PasswordProtection(
+                    getKeyStorePassword()));
+
+        FileOutputStream fos = null;
+        try {
+            File ksFile = getKeyStoreFile();
+            fos = new java.io.FileOutputStream(ksFile);
+            ks.store(fos, getKeyStorePassword());
+        } finally {
+            if (fos != null) {
+                fos.close();
+            }
+        }
+    }
+
+    public static Key generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        return kgen.generateKey();
+    }
+
+    public static File getDefaultKeyStoreLocation() {
+        String ksLocation = System.getProperty("dbfit.keystore.path");
+        if (ksLocation == null) {
+            ksLocation = System.getProperty("user.home");
+        }
+
+        return new File(ksLocation);
+    }
+
+}
+
---
+++ dbfit-java/core/src/test/java/dbfit/util/crypto/CryptoKeyStoreManagerTest.java
@@ -0,0 +1,36 @@
+package dbfit.util.crypto;
+
+import javax.crypto.KeyGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.Key;
+import javax.crypto.SecretKey;
+import java.security.KeyStore;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import static org.junit.Assert.assertNotNull;
+
+public class CryptoKeyStoreManagerTest {
+
+    @Rule public TemporaryFolder tempKeyStoreFolder = new TemporaryFolder();
+    private CryptoKeyStoreManager ksManager;
+
+    @Before
+    public void prepare() {
+        ksManager = new CryptoKeyStoreManager(tempKeyStoreFolder.getRoot());
+    }
+
+    @Test
+    public void createKeyStoreTest() throws Exception {
+        ksManager.createKeyStore();
+    }
+
+    @Test
+    public void generateKeyTest() throws NoSuchAlgorithmException {
+        assertNotNull(ksManager.generateKey());
+    }
+
+}
+
