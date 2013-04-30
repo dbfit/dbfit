eb957d2d2598339df35160d066c7d3967d359522
Christian Kraemer <spam@crasu.de>
2013-05-01 00:33:53 +0200

    tested installation script and made it run without prompting for any inputs

--- dbfit-java/oracle/src/test/resources/install_oracle.sh
+++ dbfit-java/oracle/src/test/resources/install_oracle.sh
@@ -2,10 +2,12 @@
 RPM_PATH=/var/dbfit/
 ORACLE_ENV_SCRIPT=/u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
 ORACLE_SQL_SCRIPT=/var/dbfit/dbfit-java/oracle/src/test/resources/acceptancescripts-Oracle.sql 
-ORACLE_PW
+ORACLE_PW=oracle
+ORACLE_ZIP=$(ls $RPM_PATH/oracle-xe-*.rpm.zip)
 
-if [[ -f $RPM_PATH/oracle-xe-*.rpm.zip ]]; then
-    unzip $RPM_PATH/oracle-xe-*.rpm.zip
+if [[ -f $ORACLE_ZIP ]]; then
+    echo "Oracle rpm found unzipping ..."
+    unzip -o $RPM_PATH/oracle-xe-*.rpm.zip
 else
     echo 
     echo "Go to http://www.oracle.com/technetwork/products/express-edition/downloads/index.html, download oracle-xe-*.rpm"
@@ -14,7 +16,7 @@ else
     exit 1  
 fi
 
-sudo yum install $RPM_PATH/oracle-xe-*.rpm
+sudo yum install -y $RPM_PATH/Disk1/oracle-xe-*.rpm
 if [[ $? != 0 ]]; then
     echo "rpm installation failed"
     exit 1
@@ -30,3 +32,4 @@ EOF
 
 sudo -u oracle /bin/sh -c ". $ORACLE_ENV_SCRIPT; sqlplus /nolog @$ORACLE_SQL_SCRIPT"
 
+rm -rf $RPM_PATH/Disk1
