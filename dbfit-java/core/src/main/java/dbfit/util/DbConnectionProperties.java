e0badd084b8e90e60bf71ed767cfb78ae9a5a8e4
Yavor Nikolov <nikolov.javor@gmail.com>
2013-05-03 21:12:33 +0300

    Refactor DbConnectionProperties to delegate to PropertiesLoader

--- dbfit-java/core/src/main/java/dbfit/util/DbConnectionProperties.java
+++ dbfit-java/core/src/main/java/dbfit/util/DbConnectionProperties.java
@@ -1,15 +1,12 @@
 package dbfit.util;
 
-import dbfit.util.crypto.CryptoFactories;
 import dbfit.util.crypto.CryptoService;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class DbConnectionProperties {
 
@@ -22,64 +19,17 @@ public class DbConnectionProperties {
     private DbConnectionProperties() {
     }
 
-    /**
-     * Unwrap encrypted form password "ENC(password)".
-     *
-     * @return password text if wrapped in ENC(pass)
-     *         null if format is not ENC(...)
-     */
-    public static String unwrapEncryptedPassword(String encPwd) {
-        String encryptedFormPrefix = "ENC(";
-        if (encPwd.startsWith(encryptedFormPrefix)) {
-            return encPwd.substring(encryptedFormPrefix.length(),
-                                        encPwd.length() - 1);
-        } else {
-            return null;
-        }
-    }
-
-    public static String parsePassword(String pwd) {
-        String encPwd = unwrapEncryptedPassword(pwd);
-
-        if (encPwd == null) {
-            return pwd;
-        } else {
-            CryptoService crypto = CryptoFactories.getCryptoService();
-            return crypto.decrypt(encPwd);
-        }
-    }
-
-    public static String[] splitKeyVal(String line) {
-        return line.split("=", 2);
-    }
-
-    public static DbConnectionProperties CreateFromString(
-            java.util.List<String> lines) {
-        int currLine = 0;
+    private static DbConnectionProperties createFrom(Map<String, String> map) {
         DbConnectionProperties props = new DbConnectionProperties();
-        for (String line : lines) {
-            currLine++;
-            if (line == null)
-                continue;
-            String trimline = line.trim();
-            if (trimline.length() == 0)
-                continue;
-            if (trimline.startsWith("#"))
-                continue;
-            String[] keyval = splitKeyVal(trimline);
-            String key, val;
 
-            if (keyval.length == 1) {
-                key = keyval[0].trim().toLowerCase();
-                val = "";
-            } else {
-                key = keyval[0].trim().toLowerCase();
-                val = keyval[1].trim();
-            }
+        for (Map.Entry<String, String> entry: map.entrySet()) {
+            String key = entry.getKey();
+            String val = entry.getValue();
+
             if ("username".equals(key)) {
                 props.Username = val;
             } else if ("password".equals(key)) {
-                props.Password = parsePassword(val);
+                props.Password = val;
             } else if ("service".equals(key)) {
                 props.Service = val;
             } else if ("database".equals(key)) {
@@ -90,27 +40,42 @@ public class DbConnectionProperties {
                 throw new UnsupportedOperationException(
                         "Unsupported key in properties file:" + key);
             }
-
         }
-        if (props.FullConnectionString != null)
+
+        if (props.FullConnectionString != null) {
             return props;
+        }
+
         if (props.Service != null && props.Username != null
-                && props.Password != null)
+                && props.Password != null) {
             return props;
+        }
+
         throw new Error(
                 "You have to define either the full connection string; or service, username and password in the properties file");
     }
 
+    public static DbConnectionProperties CreateFromString(List<String> lines) {
+        return CreateFromString(lines, null);
+    }
+
+    public static DbConnectionProperties CreateFromString(List<String> lines,
+            CryptoService crypto) {
+        return createFrom(getLoader(crypto).loadFromList(lines));
+    }
+
     public static DbConnectionProperties CreateFromFile(String path)
             throws FileNotFoundException, IOException {
-        File config = new File(path);
-        BufferedReader br = new BufferedReader(new FileReader(config));
-        List<String> lines = new ArrayList<String>();
-        String line;
-        while ((line = br.readLine()) != null) {
-            lines.add(line);
+
+        return createFrom(getLoader(null).loadFromFile(path));
+    }
+
+    private static PropertiesLoader getLoader(CryptoService crypto) {
+        if (crypto == null) {
+            return new PropertiesLoader();
+        } else {
+            return new PropertiesLoader(crypto);
         }
-        return CreateFromString(lines);
     }
 }
 
