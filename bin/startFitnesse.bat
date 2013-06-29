139d58debaca6a24adae86fc1a994d151ce8b537
Jake Benilov <benilov@gmail.com>
2013-06-30 00:28:46 +0100

    bump fitnesse to v20130530 (official release)

--- bin/startFitnesse.bat
+++ bin/startFitnesse.bat
@@ -1,2 +1,3 @@
-java -jar lib\fitnesse-standalone.jar -p 8085 -e 0 -o %1 %2 %3 %4 %5 
+cd /d %~dp0
+java -cp "lib\*" fitnesseMain.FitNesseMain -p 8085 -e 0 -o %1 %2 %3 %4 %5
 pause
--- bin/startFitnesse.sh
+++ bin/startFitnesse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-java -jar lib/fitnesse-standalone.jar -p 8085 -e 0 -o $1 $2 $3 $4 $5
+java -cp "lib/*" fitnesseMain.FitNesseMain -p 8085 -e 0 -o $1 $2 $3 $4 $5
 
 
