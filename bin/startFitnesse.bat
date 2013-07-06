91040d94adbe96d375bc2ccc85d21744434b5fc7
Yavor Nikolov <nikolov.javor@gmail.com>
2013-07-07 00:15:46 +0300

    Modify startFitnesse scripts so that dbfit-docs is first in classpath (In order to override updateList of fitnesse updater to take DbFit wikidocs)

--- bin/startFitnesse.bat
+++ bin/startFitnesse.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
-java -cp "lib\*" fitnesseMain.FitNesseMain -p 8085 -e 0 -o %1 %2 %3 %4 %5
+java -cp "lib\dbfit-docs.jar;lib\*" fitnesseMain.FitNesseMain -p 8085 -e 0 -o %1 %2 %3 %4 %5
 pause
