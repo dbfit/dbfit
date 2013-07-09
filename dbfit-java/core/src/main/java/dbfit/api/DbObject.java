b64de3c2e94db5476ddf53f0683aa213fa6f16e3
Jake Benilov <benilov@gmail.com>
2013-07-09 13:43:22 +0100

    reformat DbObject

--- dbfit-java/core/src/main/java/dbfit/api/DbObject.java
+++ dbfit-java/core/src/main/java/dbfit/api/DbObject.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import static dbfit.util.DbParameterAccessor.Direction;
 
 public interface DbObject {
-	public DBEnvironment getDbEnvironment();
-	public PreparedStatement buildPreparedStatement(DbParameterAccessor accessors[]) throws SQLException ;
-	public DbParameterAccessor getDbParameterAccessor(String paramName, Direction expectedDirection) throws SQLException;
+    public DBEnvironment getDbEnvironment();
+    public PreparedStatement buildPreparedStatement(DbParameterAccessor accessors[]) throws SQLException;
+    public DbParameterAccessor getDbParameterAccessor(String paramName, Direction expectedDirection) throws SQLException;
 }
