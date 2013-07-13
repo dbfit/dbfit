af726c64dfeffb7223a73270b0f18687fdde482e
Jake Benilov <benilov@gmail.com>
2013-07-13 17:18:33 +0100

    better separation of DB and FitNesse parts in SymbolAccessQueryBinding

--- dbfit-java/core/src/main/java/dbfit/util/SymbolAccessQueryBinding.java
+++ dbfit-java/core/src/main/java/dbfit/util/SymbolAccessQueryBinding.java
@@ -3,45 +3,83 @@ package dbfit.util;
 import fit.Binding;
 import fit.Fixture;
 import fit.Parse;
+import fit.TypeAdapter;
 
 public class SymbolAccessQueryBinding extends Binding.QueryBinding {
-	public void doCell(Fixture fixture, Parse cell) {
-        ContentOfTableCell content = new ContentOfTableCell(cell.text());
-		try{
-			if (content.isSymbolSetter()){
-				Object actual=this.adapter.get();
-				dbfit.util.SymbolUtil.setSymbol(content.text(), actual);
-				cell.addToBody(Fixture.gray("= "+String.valueOf(actual)));
-//				fixture.ignore(cell);
-				return;
-			}
-			if (content.isSymbolGetter()){
-				Object actual=this.adapter.get();
-				Object expected=this.adapter.parse(content.text());
-				cell.addToBody(Fixture.gray("= "+String.valueOf(expected)));
-				if (adapter.equals(actual,expected))
-					fixture.right(cell);
-				else
-					fixture.wrong(cell,String.valueOf(actual));
-				return;
-			}
-			if (content.isExpectingInequality()){
-				//expect failing comparison
-				Object actual=this.adapter.get();
-				String expectedVal=content.getExpectedFailureValue();
-				cell.addToBody(Fixture.gray("= "+String.valueOf(actual)));
-				if (adapter.equals(actual,adapter.parse(expectedVal)))
-					fixture.wrong(cell);
-				else
-					fixture.right(cell);
-				return;
-			}
-		}
-		catch (Throwable t){
-			fixture.exception(cell,t);
-			return;
-		}
-		super.doCell(fixture,cell);
+    public static interface TestResultHandler {
+        void pass();
+        void fail(String actualValue);
+        void exception(Throwable e);
+        void annotate(String message);
+    }
+
+    public static class CellTest {
+        private TestResultHandler resultHandler;
+
+        public CellTest(TestResultHandler resultHandler) {
+            this.resultHandler = resultHandler;
+        }
+
+        public void test(String expectedString, TypeAdapter adapter) {
+            ContentOfTableCell content = new ContentOfTableCell(expectedString);
+            try{
+                if (content.isSymbolSetter()){
+                    Object actual=adapter.get();
+                    dbfit.util.SymbolUtil.setSymbol(content.text(), actual);
+                    resultHandler.annotate("= " + String.valueOf(actual));
+                } else if (content.isSymbolGetter()){
+                    Object actual=adapter.get();
+                    Object expected=adapter.parse(content.text());
+                    resultHandler.annotate("= " + String.valueOf(expected));
+                    if (adapter.equals(actual,expected))
+                        resultHandler.pass();
+                    else
+                        resultHandler.fail(String.valueOf(actual));
+                } else if (content.isExpectingInequality()){
+                    //expect failing comparison
+                    Object actual=adapter.get();
+                    String expectedVal=content.getExpectedFailureValue();
+                    resultHandler.annotate("= " + String.valueOf(actual));
+                    if (adapter.equals(actual,adapter.parse(expectedVal)))
+                        resultHandler.fail(String.valueOf(actual));
+                    else
+                        resultHandler.pass();
+                } else if (content.isEmpty()) {
+                    Object actual=adapter.get();
+                    resultHandler.annotate(actual.toString());
+                } else {
+                    Object actual=adapter.get();
+                    Object expected=adapter.parse(content.text());
+                    if (adapter.equals(actual,expected))
+                        resultHandler.pass();
+                    else
+                        resultHandler.fail(String.valueOf(actual));
+                }
+            }
+            catch (Throwable t){
+                resultHandler.exception(t);
+            }
+        }
+    }
+
+	public void doCell(final Fixture fixture, final Parse cell) {
+        new CellTest(new TestResultHandler() {
+            public void pass() {
+                fixture.right(cell);
+            }
+
+            public void fail(String actualValue) {
+                fixture.wrong(cell, actualValue);
+            }
+
+            public void exception(Throwable e) {
+                fixture.exception(cell, e);
+            }
+
+            public void annotate(String message) {
+                cell.addToBody(Fixture.gray(message));
+            }
+        }).test(cell.text(), this.adapter);
 	}
 
     static class ContentOfTableCell {
@@ -69,6 +107,10 @@ public class SymbolAccessQueryBinding extends Binding.QueryBinding {
 
         public String getExpectedFailureValue() {
             return content.substring(5, content.length()-1);
+        }
+
+        public boolean isEmpty() {
+            return content.isEmpty();
         }
     }
 }
