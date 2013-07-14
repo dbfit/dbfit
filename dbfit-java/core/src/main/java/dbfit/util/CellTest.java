4132bebd83ddbaa9989ae9c00b92ce8d62157f4b
Jake Benilov <benilov@gmail.com>
2013-07-14 22:31:15 +0000

    reduce duplication, fix formatting in CellTest

--- dbfit-java/core/src/main/java/dbfit/util/CellTest.java
+++ dbfit-java/core/src/main/java/dbfit/util/CellTest.java
@@ -12,33 +12,28 @@ public class CellTest {
     public void test(String expectedString, TypeAdapter adapter) {
         ContentOfTableCell content = new ContentOfTableCell(expectedString);
         try{
+            Object actual = adapter.get();
             if (content.isSymbolSetter()){
-                Object actual=adapter.get();
                 SymbolUtil.setSymbol(content.text(), actual);
                 resultHandler.annotate("= " + String.valueOf(actual));
             } else if (content.isSymbolGetter()){
-                Object actual=adapter.get();
-                Object expected=adapter.parse(content.text());
+                Object expected = adapter.parse(content.text());
                 resultHandler.annotate("= " + String.valueOf(expected));
                 if (adapter.equals(actual,expected))
                     resultHandler.pass();
                 else
                     resultHandler.fail(String.valueOf(actual));
             } else if (content.isExpectingInequality()){
-                //expect failing comparison
-                Object actual=adapter.get();
-                String expectedVal=content.getExpectedFailureValue();
+                String expectedVal = content.getExpectedFailureValue();
                 resultHandler.annotate("= " + String.valueOf(actual));
                 if (adapter.equals(actual,adapter.parse(expectedVal)))
                     resultHandler.fail(String.valueOf(actual));
                 else
                     resultHandler.pass();
             } else if (content.isEmpty()) {
-                Object actual=adapter.get();
                 resultHandler.annotate(actual.toString());
             } else {
-                Object actual=adapter.get();
-                Object expected=adapter.parse(content.text());
+                Object expected = adapter.parse(content.text());
                 if (adapter.equals(actual,expected))
                     resultHandler.pass();
                 else
