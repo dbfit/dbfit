b0b9c3af5f868438c0057f3895f2bec60e43d066
Jake Benilov <benilov@gmail.com>
2013-07-14 12:49:25 +0000

    reformat DbStatement

--- dbfit-java/core/src/main/java/dbfit/api/DbStatement.java
+++ dbfit-java/core/src/main/java/dbfit/api/DbStatement.java
@@ -8,28 +8,30 @@ import java.sql.SQLException;
 import static dbfit.util.DbParameterAccessor.Direction;
 
 public class DbStatement implements DbObject {
-	private DBEnvironment environment;
-	private String statementText;
-	private TestHost testHost;
-	public DbStatement() {
-		environment=DbEnvironmentFactory.getDefaultEnvironment();
-	}
-	public DbStatement(DBEnvironment environment, String statementText, TestHost testHost) {
-		this.environment = environment;
-		this.statementText = statementText;
-		this.testHost=testHost;
-	}	
-	public StatementExecution buildPreparedStatement(
-			DbParameterAccessor[] accessors) throws SQLException {
-		return new StatementExecution(environment.createStatementWithBoundFixtureSymbols(testHost, statementText), false);
-	}
-	public DbParameterAccessor getDbParameterAccessor(String paramName, Direction expectedDirection){
-		return null;
-	}
+    private DBEnvironment environment;
+    private String statementText;
+    private TestHost testHost;
+
+    public DbStatement() {
+        environment = DbEnvironmentFactory.getDefaultEnvironment();
+    }
+
+    public DbStatement(DBEnvironment environment, String statementText, TestHost testHost) {
+        this.environment = environment;
+        this.statementText = statementText;
+        this.testHost = testHost;
+    }
+
+    public StatementExecution buildPreparedStatement(DbParameterAccessor[] accessors) throws SQLException {
+        return new StatementExecution(environment.createStatementWithBoundFixtureSymbols(testHost, statementText), false);
+    }
+
+    public DbParameterAccessor getDbParameterAccessor(String paramName, Direction expectedDirection) {
+        return null;
+    }
 
     @Override
     public int getExceptionCode(SQLException e) {
         return environment.getExceptionCode(e);
     }
-
 }
