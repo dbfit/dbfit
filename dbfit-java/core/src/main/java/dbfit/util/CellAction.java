3ec9675226219792e4eb58111280e8283eaaba72
Jake Benilov <benilov@gmail.com>
2013-08-11 13:48:00 +0100

    CellAction: remove unused method

--- dbfit-java/core/src/main/java/dbfit/util/CellAction.java
+++ dbfit-java/core/src/main/java/dbfit/util/CellAction.java
@@ -1,7 +1,5 @@
 package dbfit.util;
 
-import fit.TypeAdapter;
-
 public class CellAction {
     private TestResultHandler resultHandler;
 
@@ -34,42 +32,6 @@ public class CellAction {
                 resultHandler.annotate(actual.toString());
             } else {
                 Object expected = parseHelper.parse(content.text());
-                if (equals(actual,expected))
-                    resultHandler.pass();
-                else
-                    resultHandler.fail(String.valueOf(actual));
-            }
-        }
-        catch (Throwable t){
-            resultHandler.exception(t);
-        }
-    }
-
-    public void test(String expectedString, TypeAdapter adapter) {
-        ContentOfTableCell content = new ContentOfTableCell(expectedString);
-        try{
-            Object actual = adapter.get();
-            if (content.isSymbolSetter()){
-                SymbolUtil.setSymbol(content.text(), actual);
-                resultHandler.annotate("= " + String.valueOf(actual));
-            } else if (content.isSymbolGetter()){
-                Object expected = adapter.parse(content.text());
-                resultHandler.annotate("= " + String.valueOf(expected));
-                if (equals(actual,expected))
-                    resultHandler.pass();
-                else
-                    resultHandler.fail(String.valueOf(actual));
-            } else if (content.isExpectingInequality()){
-                String expectedVal = content.getExpectedFailureValue();
-                resultHandler.annotate("= " + String.valueOf(actual));
-                if (equals(actual,adapter.parse(expectedVal)))
-                    resultHandler.fail(String.valueOf(actual));
-                else
-                    resultHandler.pass();
-            } else if (content.isEmpty()) {
-                resultHandler.annotate(actual.toString());
-            } else {
-                Object expected = adapter.parse(content.text());
                 if (equals(actual,expected))
                     resultHandler.pass();
                 else
