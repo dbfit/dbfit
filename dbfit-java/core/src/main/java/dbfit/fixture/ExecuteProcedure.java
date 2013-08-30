71ed4d4c260a843f8911ce434088bb54a4af48f7
Jake Benilov <benilov@gmail.com>
2013-08-30 15:18:16 +0100

    removing DbObjectExecutionFixture

--- dbfit-java/core/src/main/java/dbfit/fixture/ExecuteProcedure.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/ExecuteProcedure.java
@@ -7,10 +7,12 @@ import dbfit.api.DbStoredProcedure;
 import dbfit.util.actions.AnyExceptionRowAction;
 import dbfit.util.actions.RowAction;
 import dbfit.util.actions.SpecificExceptionRowAction;
+import fit.Fixture;
+import fit.Parse;
 
 import java.sql.SQLException;
 
-public class ExecuteProcedure extends DbObjectExecutionFixture {
+public class ExecuteProcedure extends Fixture {
     public enum Expectation {
         SUCCESS,
         ANY_EXCEPTION,
@@ -24,6 +26,34 @@ public class ExecuteProcedure extends DbObjectExecutionFixture {
 
         public Integer getExpectedErrorCode() {
             return expectedErrorCode;
+        }
+    }
+
+    public static class ExecuteProcedureTable extends ExecutionTable {
+        private DBEnvironment environment;
+        private String procedureName;
+        private Expectation expectation;
+
+        public ExecuteProcedureTable(DBEnvironment environment, String procedureName, Expectation expectation, Fixture fixture, Parse rows) {
+            super(fixture, rows);
+            this.environment = environment;
+            this.procedureName = procedureName;
+            this.expectation = expectation;
+        }
+
+        @Override
+        protected DbObject getTargetDbObject() throws SQLException {
+            return new DbStoredProcedure(environment, procedureName);
+        }
+
+        @Override
+        protected RowAction newRowAction(StatementExecution execution) {
+            switch (expectation) {
+                case SUCCESS: return new RowAction(execution);
+                case ANY_EXCEPTION: return new AnyExceptionRowAction(execution);
+                case SPECIFIC_EXCEPTION: return new SpecificExceptionRowAction(execution, expectation.getExpectedErrorCode());
+                default: throw new RuntimeException("Internal error: expectation not set");
+            }
         }
     }
 
@@ -56,18 +86,13 @@ public class ExecuteProcedure extends DbObjectExecutionFixture {
         expectation = Expectation.SUCCESS;
     }
 
-    @Override
-    protected DbObject getTargetDbObject() throws SQLException {
-        return new DbStoredProcedure(environment, getProcedureName());
-    }
-
-    @Override
-    protected RowAction newRowTest(StatementExecution execution) {
-        switch (expectation) {
-            case SUCCESS: return new RowAction(execution);
-            case ANY_EXCEPTION: return new AnyExceptionRowAction(execution);
-            case SPECIFIC_EXCEPTION: return new SpecificExceptionRowAction(execution, expectation.getExpectedErrorCode());
-            default: throw new RuntimeException("Internal error: expectation not set");
+    public void doRows(Parse rows) {
+        try {
+            new ExecuteProcedureTable(environment, getProcedureName(), expectation, this, rows).run();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            if (rows == null) throw new Error(e);
+            exception(rows.parts, e);
         }
     }
 
--- dbfit-java/core/src/main/java/dbfit/fixture/Insert.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/Insert.java
@@ -4,10 +4,12 @@ import dbfit.api.DBEnvironment;
 import dbfit.api.DbEnvironmentFactory;
 import dbfit.api.DbObject;
 import dbfit.api.DbTable;
+import fit.Fixture;
+import fit.Parse;
 
 import java.sql.SQLException;
 
-public class Insert extends DbObjectExecutionFixture {
+public class Insert extends Fixture {
     private DBEnvironment environment;
     private String tableName;
 
@@ -24,11 +26,37 @@ public class Insert extends DbObjectExecutionFixture {
         this.environment = dbEnvironment;
     }
 
-    @Override
-    protected DbObject getTargetDbObject() throws SQLException {
+    public void doRows(Parse rows) {
+        try {
+            new InsertTable(environment, getTableName(), this, rows).run();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            if (rows == null) throw new Error(e);
+            exception(rows.parts, e);
+        }
+    }
+
+    public static class InsertTable extends ExecutionTable {
+        private DBEnvironment environment;
+        private String tableName;
+
+        public InsertTable(DBEnvironment environment, String tableName, Fixture fixture, Parse rows) {
+            super(fixture, rows);
+
+            this.environment = environment;
+            this.tableName = tableName;
+        }
+
+        @Override
+        protected DbObject getTargetDbObject() throws SQLException {
+            return new DbTable(environment, tableName);
+        }
+    }
+
+    protected String getTableName() {
         if ((tableName == null || tableName.trim().length() == 0) && args.length > 0) {
             tableName = args[0];
         };
-        return new DbTable(environment, tableName);
+        return tableName;
     }
 }
