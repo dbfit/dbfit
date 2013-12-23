284dca56f4e53ae99c02a49ad84148816a88b5e1
Yavor Nikolov <nikolov.javor@gmail.com>
2013-12-23 20:54:07 +0200

    DataColumn - whitespace reformat

--- dbfit-java/core/src/main/java/dbfit/util/DataColumn.java
+++ dbfit-java/core/src/main/java/dbfit/util/DataColumn.java
@@ -8,26 +8,32 @@ import java.sql.SQLException;
  * @see DataTable
  */
 public class DataColumn {
-	private String name;
-	private String javaClassName;
-	private String dbTypeName;
-	public DataColumn(String name, String javaClassName, String dbTypeName) {
-		this.name = name;
-		this.javaClassName = javaClassName;
-		this.dbTypeName = dbTypeName;
-	}
-	public DataColumn(ResultSetMetaData r, int columnIndex) throws SQLException{
-		this.name=r.getColumnLabel(columnIndex);
-		this.javaClassName=r.getColumnClassName(columnIndex);
-		this.dbTypeName=r.getColumnTypeName(columnIndex);
-	}
-	public String getDbTypeName() {
-		return dbTypeName;
-	}
-	public String getJavaClassName() {
-		return javaClassName;
-	}
-	public String getName() {
-		return name;
-	}
+    private String name;
+    private String javaClassName;
+    private String dbTypeName;
+
+    public DataColumn(String name, String javaClassName, String dbTypeName) {
+        this.name = name;
+        this.javaClassName = javaClassName;
+        this.dbTypeName = dbTypeName;
+    }
+
+    public DataColumn(ResultSetMetaData r, int columnIndex) throws SQLException {
+        this.name = r.getColumnLabel(columnIndex);
+        this.javaClassName = r.getColumnClassName(columnIndex);
+        this.dbTypeName = r.getColumnTypeName(columnIndex);
+    }
+
+    public String getDbTypeName() {
+        return dbTypeName;
+    }
+
+    public String getJavaClassName() {
+        return javaClassName;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
