7b3b3fd0e43991e59cde43ec19a872c7ec1e6a4d
Yavor Nikolov <nikolov.javor@gmail.com>
2014-01-12 01:07:40 +0200

    Add some unit tests for MatchableDataTable

---
+++ dbfit-java/core/src/test/java/dbfit/util/MatchableDataTableTest.java
@@ -0,0 +1,107 @@
+package dbfit.util;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import java.util.Map;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+import static java.util.Arrays.asList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MatchableDataTableTest {
+
+    @Mock private DataRow r1;
+    @Mock private DataRow r2;
+    @Mock private DataRow r3;
+    @Mock private DataRow r4;
+    @Mock private DataTable mockedDataTable;
+
+    @Mock private Map<String,Object> matchingProperties;
+
+    private List<DataRow> rows;
+    private MatchableDataTable mdt;
+
+    @Before
+    public void prepare() {
+        when(r3.matches(matchingProperties)).thenReturn(true);
+
+        rows = asList(r1, r2, r3, r4);
+        when(mockedDataTable.getRows()).thenReturn(rows);
+        mdt = new MatchableDataTable(mockedDataTable);
+    }
+
+    @Test
+    public void initiallyAllRowsShouldBeUnprocessed() {
+        assertEquals(mdt.getUnprocessedRows(), rows);
+    }
+
+    @Test
+    public void markShouldRemoveItFromUnprocessedR1() {
+        mdt.markProcessed(r1);
+        assertEquals(asList(r2, r3, r4), mdt.getUnprocessedRows());
+    }
+
+    @Test
+    public void markShouldRemoveItFromUnprocessedR2() {
+        mdt.markProcessed(r2);
+        assertEquals(asList(r1, r3, r4), mdt.getUnprocessedRows());
+    }
+
+    @Test
+    public void findFirstUnprocessedRowShouldBeR1() throws NoMatchingRowFoundException {
+        DataRow firstUnprocessed = mdt.findFirstUnprocessedRow();
+        assertEquals(r1, firstUnprocessed);
+    }
+
+    @Test
+    public void findFirstUnprocessedRowShouldBeR2() throws NoMatchingRowFoundException {
+        mdt.markProcessed(r1);
+
+        DataRow firstUnprocessed = mdt.findFirstUnprocessedRow();
+        assertEquals(r2, firstUnprocessed);
+    }
+
+    @Test
+    public void findMatchingShouldBeR3() throws NoMatchingRowFoundException {
+        DataRow matchingRow = mdt.findMatching(matchingProperties);
+
+        assertEquals(r3, matchingRow);
+    }
+
+    @Test(expected = NoMatchingRowFoundException.class)
+    public void findMatchingOfProcessedRowShouldRaiseException() throws NoMatchingRowFoundException {
+        mdt.markProcessed(r3);
+
+        DataRow matchingRow = mdt.findMatching(matchingProperties);
+    }
+
+    @Test
+    public void shouldBeAbleToRemoveUnprocessedViaIterator() {
+        Iterator iter = mdt.getUnprocessedRows().iterator();
+        iter.next();
+        iter.remove();
+
+        assertEquals(asList(r2, r3, r4), mdt.getUnprocessedRows());
+    }
+
+    @Test
+    public void shouldDelegateGetColumnsToDataTable() {
+        mdt.getColumns();
+
+        verify(mockedDataTable).getColumns();
+    }
+
+}
+
