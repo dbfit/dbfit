293a803d0961ad9c2d83437c1ededd39f87174c1
Yavor Nikolov <nikolov.javor@gmail.com>
2014-01-12 14:20:23 +0200

    reformat Query fixture

--- dbfit-java/core/src/main/java/dbfit/fixture/Query.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/Query.java
@@ -11,53 +11,59 @@ import java.sql.SQLException;
 import static dbfit.util.SymbolUtil.isSymbolGetter;
 
 public class Query extends RowSetFixture {
-	private DBEnvironment dbEnvironment;
-	private String queryOrSymbol;
-	private boolean isOrdered;
+    private DBEnvironment dbEnvironment;
+    private String queryOrSymbol;
+    private boolean isOrdered;
 
-	public Query() {
-		dbEnvironment = DbEnvironmentFactory.getDefaultEnvironment();
-		isOrdered = false;
-	}
+    public Query() {
+        dbEnvironment = DbEnvironmentFactory.getDefaultEnvironment();
+        isOrdered = false;
+    }
 
-	public Query(DBEnvironment environment, String queryOrSymbol) {
-		this(environment, queryOrSymbol, false);
-	}
+    public Query(DBEnvironment environment, String queryOrSymbol) {
+        this(environment, queryOrSymbol, false);
+    }
 
-	public Query(DBEnvironment environment, String queryOrSymbol, boolean isOrdered) {
-		this.dbEnvironment = environment;
-		this.queryOrSymbol = queryOrSymbol;
-		this.isOrdered = isOrdered;
-	}
+    public Query(DBEnvironment environment, String queryOrSymbol, boolean isOrdered) {
+        this.dbEnvironment = environment;
+        this.queryOrSymbol = queryOrSymbol;
+        this.isOrdered = isOrdered;
+    }
 
-	public MatchableDataTable getDataTable() throws SQLException {
-		if (queryOrSymbol == null)
-			queryOrSymbol = args[0];
-		if (isSymbolGetter(queryOrSymbol))
-			return new MatchableDataTable(getFromSymbol());
-		Log.log("Query: '%s'", queryOrSymbol);
-		PreparedStatement st = dbEnvironment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), queryOrSymbol);
-		return new MatchableDataTable(new DataTable(st.executeQuery()));
-	}
+    public MatchableDataTable getDataTable() throws SQLException {
+        if (queryOrSymbol == null) {
+            queryOrSymbol = args[0];
+        }
 
-	private DataTable getFromSymbol() throws SQLException {
-		Object o = dbfit.util.SymbolUtil.getSymbol(queryOrSymbol);
-		if (o instanceof ResultSet) {
-			return new DataTable((ResultSet) o);
-		} else if (o instanceof DataTable) {
-			return (DataTable) o;
-		}
-		throw new UnsupportedOperationException("Stored queries can only be used on symbols that contain result sets");
-	}
+        if (isSymbolGetter(queryOrSymbol)) {
+            return new MatchableDataTable(getFromSymbol());
+        }
 
-	protected boolean isOrdered() {
-		return isOrdered;
-	}
+        Log.log("Query: '%s'", queryOrSymbol);
+        PreparedStatement st = dbEnvironment.createStatementWithBoundFixtureSymbols(FitNesseTestHost.getInstance(), queryOrSymbol);
+        return new MatchableDataTable(new DataTable(st.executeQuery()));
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	protected Class getJavaClassForColumn(DataColumn col) throws ClassNotFoundException, SQLException {
-		// System.out.println(col.getName()+":"+col.getJavaClassName());
-		return dbEnvironment.getJavaClass(col.getDbTypeName());
-	}
+    private DataTable getFromSymbol() throws SQLException {
+        Object o = dbfit.util.SymbolUtil.getSymbol(queryOrSymbol);
+
+        if (o instanceof ResultSet) {
+            return new DataTable((ResultSet) o);
+        } else if (o instanceof DataTable) {
+            return (DataTable) o;
+        }
+
+        throw new UnsupportedOperationException("Stored queries can only be used on symbols that contain result sets");
+    }
+
+    protected boolean isOrdered() {
+        return isOrdered;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected Class getJavaClassForColumn(DataColumn col) throws ClassNotFoundException, SQLException {
+        return dbEnvironment.getJavaClass(col.getDbTypeName());
+    }
 }
+
