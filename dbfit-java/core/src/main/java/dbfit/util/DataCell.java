6713854a8181a7bd5bc40e15e0e00d6132ac417f
Yavor Nikolov <nikolov.javor@gmail.com>
2014-01-23 03:03:22 +0200

    Implemented DataCell

--- dbfit-java/core/src/main/java/dbfit/util/DataCell.java
+++ dbfit-java/core/src/main/java/dbfit/util/DataCell.java
@@ -1,5 +1,28 @@
 package dbfit.util;
 
 public class DataCell {
-}
+    private DataRow row;
+    private String columnName;
 
+    public DataCell(final DataRow row, final String columnName) {
+        this.row = row;
+        this.columnName = columnName;
+    }
+
+    /**
+     *
+     * @return new instance if row is non-null or return null otherwise.
+     */
+    public static DataCell createDataCell(final DataRow row, final String col) {
+        return (row == null) ? null : new DataCell(row, col);
+    }
+
+    @Override
+    public String toString() {
+        return row.getStringValue(columnName);
+    }
+
+    public boolean equalTo(final DataCell cell2) {
+        return (cell2 != null) && toString().equals(cell2.toString());
+    }
+}
