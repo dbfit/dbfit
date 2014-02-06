45ae09f680df740b90fa55202a52488deac5a386
Yavor Nikolov <nikolov.javor@gmail.com>
2014-02-06 23:16:40 +0200

    Refactor DataTableDiff: extend CompositeDiff

--- dbfit-java/core/src/main/java/dbfit/diff/DataTableDiff.java
+++ dbfit-java/core/src/main/java/dbfit/diff/DataTableDiff.java
@@ -3,16 +3,12 @@ package dbfit.diff;
 import dbfit.util.MatchableDataTable;
 import dbfit.util.DataTable;
 import dbfit.util.DataRow;
-import dbfit.util.DiffResultsSummarizer;
-import dbfit.util.MatchResult;
 import dbfit.util.RowStructure;
 import dbfit.util.DataRowProcessor;
 import dbfit.util.NoMatchingRowFoundException;
 import dbfit.util.MatchingMaskBuilder;
 
-import java.util.List;
-
-public class DataTableDiff extends DiffBase<DataTable, DataTable> {
+public class DataTableDiff extends CompositeDiff<DataTable, DataRow> {
 
     private RowStructure rowStructure;
 
@@ -20,21 +16,48 @@ public class DataTableDiff extends DiffBase<DataTable, DataTable> {
         this.rowStructure = rowStructure;
     }
 
-    class DataTablesMatchProcessor implements DataRowProcessor {
-        MatchableDataTable mdt2;
-        public DiffResultsSummarizer summer;
-        private MatchingMaskBuilder mmb = new MatchingMaskBuilder(rowStructure);
+    @Override
+    protected Class getType() {
+        return DataTable.class;
+    }
 
-        public DataTablesMatchProcessor(
-                final DataTable table2,
-                final DiffResultsSummarizer summer) {
+    @Override
+    protected Class getChildType() {
+        return DataRow.class;
+    }
+
+    @Override
+    DiffRunner getDiffRunner(final DataTable dr1, final DataTable dr2) {
+        return new DataTableDiffRunner(dr1, dr2);
+    }
+
+    class DataTableDiffRunner extends CompositeDiffRunner implements DataRowProcessor {
+
+        private MatchingMaskBuilder mmb = new MatchingMaskBuilder(rowStructure);
+        private MatchableDataTable mdt2;
+
+        public DataTableDiffRunner(final DataTable table1, final DataTable table2) {
+            super(table1, table2);
             this.mdt2 = new MatchableDataTable(table2);
-            this.summer = summer;
+        }
+
+        @Override
+        protected DataRowDiff newChildDiff() {
+            return new DataRowDiff(rowStructure.getColumnNames());
+        }
+
+        @Override
+        protected void uncheckedDiff() {
+            new MatchableDataTable(o1).processDataRows(this);
+
+            for (DataRow dr: mdt2.getUnprocessedRows()) {
+                createChildDiff().diff(null, dr);
+            }
         }
 
         @Override
         public void process(DataRow row1) {
-            DataRowDiff rowDiff = createChildDiff(summer);
+            DiffBase<DataRow, DataRow> rowDiff = createChildDiff();
 
             try {
                 DataRow row2 = mdt2.findMatching(mmb.buildMatchingMask(row1));
@@ -45,56 +68,6 @@ public class DataTableDiff extends DiffBase<DataTable, DataTable> {
             }
         }
 
-        public List<DataRow> getUnprocessedRows() {
-            return mdt2.getUnprocessedRows();
-        }
-    }
-
-    @Override
-    public void diff(final DataTable table1, final DataTable table2) {
-        new DataTableDiffRunner(table1, table2).runDiff();
-    }
-
-    private DiffResultsSummarizer createSummer(final DataTable table1,
-                                               final DataTable table2) {
-        return new DiffResultsSummarizer(
-                MatchResult.create(table1, table2, DataTable.class),
-                DataRow.class);
-    }
-
-    private DataRowDiff createChildDiff(final DiffResultsSummarizer summer) {
-        DataRowDiff diff = new DataRowDiff(rowStructure.getColumnNames());
-        diff.addListeners(listeners);
-        diff.addListener(summer);
-        return diff;
-    }
-
-    class DataTableDiffRunner extends DiffRunner {
-        private final DataTable table1;
-        private final DataTable table2;
-        private final DiffResultsSummarizer summer;
-
-        public DataTableDiffRunner(final DataTable table1, final DataTable table2) {
-            this.table1 = table1;
-            this.table2 = table2;
-            this.summer = createSummer(table1, table2);
-        }
-
-        @Override public MatchResult getResult() {
-            return summer.getResult();
-        }
-
-        @Override
-        protected void uncheckedDiff() {
-            DataTablesMatchProcessor processor = new DataTablesMatchProcessor(
-                    table2, summer);
-
-            new MatchableDataTable(table1).processDataRows(processor);
-
-            for (DataRow dr: processor.getUnprocessedRows()) {
-                createChildDiff(summer).diff(null, dr);
-            }
-        }
     }
 
 }
