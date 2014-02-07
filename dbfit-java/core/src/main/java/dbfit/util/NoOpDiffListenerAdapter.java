fd11e63fc00e9d5628b3788830df6365f81e148f
Yavor Nikolov <nikolov.javor@gmail.com>
2014-02-07 19:59:21 +0200

    Remove unused start* events from DiffHandler

--- dbfit-java/core/src/main/java/dbfit/util/NoOpDiffListenerAdapter.java
+++ dbfit-java/core/src/main/java/dbfit/util/NoOpDiffListenerAdapter.java
@@ -8,19 +8,10 @@ public class NoOpDiffListenerAdapter extends DiffListenerAdapter
     }
 
     @Override
-    public void startTable(MatchResult<DataTable, DataTable> result) {}
-
-    @Override
     public void endTable(MatchResult<DataTable, DataTable> result) {}
 
     @Override
-    public void startRow(MatchResult<DataRow, DataRow> result) {}
-
-    @Override
     public void endRow(MatchResult<DataRow, DataRow> result) {}
-
-    @Override
-    public void startCell(MatchResult<DataCell, DataCell> result) {}
 
     @Override
     public void endCell(MatchResult<DataCell, DataCell> result) {}
