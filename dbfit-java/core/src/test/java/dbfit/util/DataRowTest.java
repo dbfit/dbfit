7717221f912afb6cc9a5c0260c73f46b74610439
Yavor Nikolov <nikolov.javor@gmail.com>
2014-02-08 16:30:01 +0200

    DataRowTest: null-cases test of getStringValue

--- dbfit-java/core/src/test/java/dbfit/util/DataRowTest.java
+++ dbfit-java/core/src/test/java/dbfit/util/DataRowTest.java
@@ -1,19 +1,32 @@
 package dbfit.util;
 
+import dbfit.util.DiffTestUtils.DataRowBuilder;
 import static dbfit.util.DiffTestUtils.createDataRowBuilder;
 
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
 
 public class DataRowTest {
 
-    private DataRow row = createDataRowBuilder().createRow(2, 4);
+    private final DataRowBuilder rb = createDataRowBuilder();
+    private DataRow row = rb.createRow(2, 4);
 
     @Test
     public void testDataRowStringValueOfExistingColumns() {
         assertThat(row.getStringValue("c0"), is("2"));
         assertThat(row.getStringValue("c1"), is("4"));
+    }
+
+    @Test
+    public void testDataRowStringValueOfNullCells() {
+        row = rb.createRow(2, null);
+        assertThat(row.getStringValue("c1"), is("null"));
+    }
+
+    @Test
+    public void testDataRowStringValueOfMissingColumn() {
+        assertThat(row.getStringValue("NO-SUCH-COLUMN"), is("null"));
     }
 
 }
