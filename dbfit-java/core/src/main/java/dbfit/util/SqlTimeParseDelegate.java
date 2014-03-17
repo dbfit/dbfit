00a9c7170dd6b1c7952c9cb495c8d867900b64ef
Yavor Nikolov <nikolov.javor@gmail.com>
2014-03-17 22:15:35 +0200

    SqlTimeParseDelegate: milliseonds support

--- dbfit-java/core/src/main/java/dbfit/util/SqlTimeParseDelegate.java
+++ dbfit-java/core/src/main/java/dbfit/util/SqlTimeParseDelegate.java
@@ -1,7 +1,19 @@
 package dbfit.util;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
 public class SqlTimeParseDelegate {
+    static final SimpleDateFormat FMT_S = new SimpleDateFormat("HH:mm:ss");
+    static final SimpleDateFormat FMT_MS = new SimpleDateFormat("HH:mm:ss.S");
+
     public static Object parse(String s) throws Exception {
-        return java.sql.Time.valueOf(s);
+        return (s == null) ? null : parseTime(s);
+    }
+
+    private static Time parseTime(final String s) throws ParseException {
+        SimpleDateFormat df = s.contains(".") ? FMT_MS : FMT_S;
+        return new Time(df.parse(s).getTime());
     }
 }
