ca210c8cfb63d2e280f14073e8059f3ef63e7431
Yavor Nikolov <nikolov.javor@gmail.com>
2014-03-22 01:28:26 +0200

    mssql: refactor normaliseTypeName

--- dbfit-java/sqlserver/src/main/java/dbfit/environment/SqlServerTypeNameNormaliser.java
+++ dbfit-java/sqlserver/src/main/java/dbfit/environment/SqlServerTypeNameNormaliser.java
@@ -2,13 +2,6 @@ package dbfit.environment;
 
 public class SqlServerTypeNameNormaliser {
     public static String normaliseTypeName(String dataType) {
-        dataType = dataType.toUpperCase().trim();
-        int idx = dataType.indexOf(" ");
-        if (idx >= 0)
-            dataType = dataType.substring(0, idx);
-        idx = dataType.indexOf("(");
-        if (idx >= 0)
-            dataType = dataType.substring(0, idx);
-        return dataType;
+        return dataType.trim().toUpperCase().split("[\\s(]")[0];
     }
 }
