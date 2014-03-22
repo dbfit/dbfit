30fbbe9f37661e81dc79e21eca6b0e026032954e
Yavor Nikolov <nikolov.javor@gmail.com>
2014-03-23 01:07:50 +0200

    BigDecimalParseDelegate to return NormalisedBigDecimal

--- dbfit-java/core/src/main/java/dbfit/util/BigDecimalParseDelegate.java
+++ dbfit-java/core/src/main/java/dbfit/util/BigDecimalParseDelegate.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 
 public class BigDecimalParseDelegate {
     public static Object parse(String s) {
-        return new BigDecimal(s);
+        return new NormalisedBigDecimal(new BigDecimal(s));
     }
 }
