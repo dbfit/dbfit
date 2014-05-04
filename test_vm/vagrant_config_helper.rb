9ff29a1671fda1a6e2afc29ff2027f918be70163
Yavor Nikolov <nikolov.javor@gmail.com>
2014-05-04 21:47:24 +0300

    Extend vagrant *config* and *chef* customization support  - Can customize "multi-dot" settings (e.g. "config.a.b.c")  - Add customization example for proxy settings  - Change the API for advanced settings customization

--- test_vm/vagrant_config_helper.rb
+++ test_vm/vagrant_config_helper.rb
@@ -4,6 +4,16 @@ def windows?
   RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin|msys|windows/i
 end
 
+class DbFit
+  def self.customize_config(cfg_group, &block)
+    CONFIG[cfg_group] = block
+  end
+
+  def self.apply_custom_config(cfg_group, target_config)
+    CONFIG[cfg_group].call(target_config) if CONFIG[cfg_group].is_a? Proc
+  end
+end
+
 custom_config_file = File.expand_path("../vagrant_config_custom.rb", __FILE__)
 CONFIG = Hash.new
 
@@ -13,6 +23,3 @@ require_relative 'vagrant_config_defaults'
 # Load local customizations if any
 require_relative custom_config_file if File.exist?(custom_config_file)
 
-def customize_config(cfg_group, cfg)
-  CONFIG[cfg_group].each { |key, val| cfg.send("#{key}=", *val) }
-end
