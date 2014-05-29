e4ef3ecfc5cc6ff9c4b75caad0a3cb9f21de59d1
Yavor Nikolov <nikolov.javor@gmail.com>
2014-05-29 21:13:00 +0300

    Remove unused methods from TestHost

--- dbfit-java/core/src/main/java/dbfit/api/TestHost.java
+++ dbfit-java/core/src/main/java/dbfit/api/TestHost.java
@@ -2,6 +2,4 @@ package dbfit.api;
 
 public interface TestHost {
     public Object getSymbolValue(String symbolName);
-    public void setSymbolValue(String symbolName, Object value);
-    public void clearSymbols();
 }
