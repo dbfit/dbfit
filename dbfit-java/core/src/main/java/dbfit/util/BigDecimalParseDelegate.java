987d3b12d66e67ba2d310a4cd0f87ebc03860f4e
hal9ccc <hal9ccc@googlemail.com>
2014-08-11 14:37:22 +0200

    fixed java.lang.NumberFormatException when using decimal values with locales having a decimal separator other than "."

--- dbfit-java/core/src/main/java/dbfit/util/BigDecimalParseDelegate.java
+++ dbfit-java/core/src/main/java/dbfit/util/BigDecimalParseDelegate.java
@@ -1,9 +1,16 @@
 package dbfit.util;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class BigDecimalParseDelegate {
+    private static NumberFormat nf = NumberFormat.getInstance();
     public static Object parse(String s) {
-        return new NormalisedBigDecimal(new BigDecimal(s));
+        try {
+            return new NormalisedBigDecimal(new BigDecimal(nf.parse(s).toString()));
+        } catch (ParseException e) {
+            return new NormalisedBigDecimal(new BigDecimal(s));
+        }
     }
 }
