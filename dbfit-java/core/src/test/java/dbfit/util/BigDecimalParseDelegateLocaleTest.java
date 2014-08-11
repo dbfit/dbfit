26061770f28f41079c0df374982cdc506c3e3462
hal9ccc <hal9ccc@googlemail.com>
2014-08-11 18:36:08 +0200

    added test aDecimalOfLocaleFormatIsParsed

---
+++ dbfit-java/core/src/test/java/dbfit/util/BigDecimalParseDelegateLocaleTest.java
@@ -0,0 +1,21 @@
+package dbfit.util;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import static org.junit.Assert.*;
+
+public class BigDecimalParseDelegateLocaleTest {
+    @Test
+    public void aDecimalOfLocaleFormatIsParsed() {
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.getDefault());
+        BigDecimalNormaliser bdn = new BigDecimalNormaliser();
+        BigDecimal bd1 = (BigDecimal) bdn.normalise(new BigDecimal(111.111));
+        BigDecimal bd2 = (BigDecimal) bdn.normalise(BigDecimalParseDelegate.parse("111" + dfs.getDecimalSeparator() + "111"));
+        assertEquals(bd1, bd2);
+    }
+
+}
