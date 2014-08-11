9de9f78d3ae3f88cb84c5e95e631a7d3a8c5b008
hal9ccc <hal9ccc@googlemail.com>
2014-08-11 22:18:45 +0200

    usage of DecimalFormat instead of NumberFormat

--- dbfit-java/core/src/main/java/dbfit/util/BigDecimalParseDelegate.java
+++ dbfit-java/core/src/main/java/dbfit/util/BigDecimalParseDelegate.java
@@ -1,13 +1,15 @@
 package dbfit.util;
 
 import java.math.BigDecimal;
-import java.text.NumberFormat;
+import java.text.DecimalFormat;
 import java.text.ParseException;
 
 public class BigDecimalParseDelegate {
-    private static NumberFormat nf = NumberFormat.getInstance();
+    private static DecimalFormat nf = new DecimalFormat();
+
     public static Object parse(String s) {
         try {
+            nf.setParseBigDecimal(true);
             return new NormalisedBigDecimal(new BigDecimal(nf.parse(s).toString()));
         } catch (ParseException e) {
             return new NormalisedBigDecimal(new BigDecimal(s));
--- dbfit-java/core/src/test/java/dbfit/util/BigDecimalParseDelegateLocaleTest.java
+++ dbfit-java/core/src/test/java/dbfit/util/BigDecimalParseDelegateLocaleTest.java
@@ -9,13 +9,36 @@ import java.util.Locale;
 import static org.junit.Assert.*;
 
 public class BigDecimalParseDelegateLocaleTest {
+    private char ds = new DecimalFormatSymbols(Locale.getDefault()).getDecimalSeparator();
+    private BigDecimalNormaliser normaliser = new BigDecimalNormaliser();
+
     @Test
     public void aDecimalOfLocaleFormatIsParsed() {
-        DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.getDefault());
-        BigDecimalNormaliser bdn = new BigDecimalNormaliser();
-        BigDecimal bd1 = (BigDecimal) bdn.normalise(new BigDecimal("111.111"));
-        BigDecimal bd2 = (BigDecimal) bdn.normalise(BigDecimalParseDelegate.parse("111" + dfs.getDecimalSeparator() + "111"));
+        BigDecimal bd1 = normalise(new BigDecimal("111.111"));
+        BigDecimal bd2 = normalise(BigDecimalParseDelegate.parse("111" + ds + "111"));
         assertEquals(bd1, bd2);
+    }
+
+    @Test
+    public void aHugeDecimalOfLocaleFormatIsParsed() {
+        BigDecimal bd1 = normalise(new BigDecimal("123456789012345.123456789012345"));
+        BigDecimal bd2 = normalise(BigDecimalParseDelegate.parse("123456789012345" + ds + "123456789012345"));
+        assertEquals(bd1, bd2);
+    }
+
+    @Test
+    public void aNegativeDecimalOfLocaleFormatIsParsed() {
+        BigDecimal bd1 = normalise(new BigDecimal("-98765.0000111"));
+        BigDecimal bd2 = normalise(BigDecimalParseDelegate.parse("-98765" + ds + "0000111"));
+        assertEquals(bd1, bd2);
+    }
+
+    private BigDecimal normalise(BigDecimal val) {
+        return (BigDecimal) normaliser.normalise(val);
+    }
+
+    private BigDecimal normalise(Object val) {
+        return (BigDecimal) normaliser.normalise(val);
     }
 
 }
