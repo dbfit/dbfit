0843ae35b01900d77e4330097f9d4aaeae02353b
Alexey Skorobogatov <avskor@gmail.com>
2014-10-24 23:37:23 +0400

    Remove extra spaces.

--- dbfit-java/oracle/src/main/java/dbfit/environment/OracleEnvironment.java
+++ dbfit-java/oracle/src/main/java/dbfit/environment/OracleEnvironment.java
@@ -346,7 +346,7 @@ public class OracleEnvironment extends AbstractDbEnvironment {
 
         DbParameterAccessor dbp = new OracleDbParameterAccessor(paramName,
                 paramDirection, getSqlType(dataType), getJavaClass(dataType),
-                paramPosition,  normaliseTypeName(dataType), userTypeName);
+                paramPosition, normaliseTypeName(dataType), userTypeName);
 
         return dbp;
     }
