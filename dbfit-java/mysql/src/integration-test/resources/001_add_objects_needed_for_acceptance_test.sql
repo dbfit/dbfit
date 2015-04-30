9fabee62c643a4cc2711566e2fd633025603c185
Mark Matten <mark_matten@hotmail.com>
2015-04-30 16:19:09 +0000

    Migrate SQL Server to CoreTests suite. Prepare other adaptor tests for graviation towards CoreTests suite.
    Standardise update tests

--- dbfit-java/mysql/src/integration-test/resources/001_add_objects_needed_for_acceptance_test.sql
+++ dbfit-java/mysql/src/integration-test/resources/001_add_objects_needed_for_acceptance_test.sql
@@ -1,4 +1,4 @@
-create table users(name varchar(50) unique, username varchar(50), userid int auto_increment primary key) ENGINE=InnoDB;
+create table Users(name varchar(50) unique, username varchar(50), userid int auto_increment primary key) ENGINE=InnoDB;
 
 CREATE PROCEDURE ConcatenateStrings (IN firststring varchar(100), IN secondstring varchar(100), OUT concatenated varchar(200)) set concatenated = concat(firststring , concat( ' ' , secondstring ));
 
@@ -6,8 +6,8 @@ create procedure CalcLength(IN name varchar(100), OUT strlength int) set strleng
 
 CREATE FUNCTION ConcatenateF (firststring  VARCHAR(100), secondstring varchar(100)) RETURNS VARCHAR(200) DETERMINISTIC RETURN CONCAT(firststring,' ',secondstring);
 
-create procedure makeuser() insert into users (name,username) values ('user1','fromproc');
+create procedure makeuser() insert into Users (name,username) values ('user1','fromproc');
 
-create procedure createuser(IN newname varchar(100), IN newusername varchar(100)) insert into users (name,username) values (newname, newusername);
+create procedure createuser(IN newname varchar(100), IN newusername varchar(100)) insert into Users (name,username) values (newname, newusername);
 
 create procedure Multiply(IN factor int, INOUT val int) set val =val*factor;
