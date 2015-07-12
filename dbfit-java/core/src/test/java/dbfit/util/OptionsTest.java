ad1ea5b2672f0a12ac1d81b66a387536ead74c3e
Yavor Nikolov <nikolov.javor@gmail.com>
2015-07-12 13:04:07 +0300

    Refactor Options
    * Use common hash map for all options (including predefined one) to
      reduce the need to handle special cases separately
      - Pre-defined option values are parsed lazily on retrieval time
        (not when being set)
    * Use final constants for predefined option names instead of literals
    * Reset free options on reset()
    * Add OptionsTest

---
+++ dbfit-java/core/src/test/java/dbfit/util/OptionsTest.java
@@ -0,0 +1,60 @@
+package dbfit.util;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class OptionsTest {
+
+    @Before
+    public void prepare() {
+        Options.reset();
+    }
+
+    @Test
+    public void debugLogDefaultIsFalse() {
+        assertThat(Options.isDebugLog(), is(false));
+    }
+
+    @Test
+    public void fixedLengthStringParsingDefaultIsFalse() {
+        assertThat(Options.isFixedLengthStringParsing(), is(false));
+    }
+
+    @Test
+    public void bindSymbolsDefaultIsTrue() {
+        assertThat(Options.isBindSymbols(), is(true));
+    }
+
+    @Test
+    public void canSetPredefinedOption() {
+        Options.setOption(Options.OPTION_DEBUG_LOG, "true");
+        assertThat(Options.isDebugLog(), is(true));
+    }
+
+    @Test
+    public void canUnsetPredefinedOption() {
+        Options.setOption(Options.OPTION_DEBUG_LOG, "true");
+        Options.setOption(Options.OPTION_DEBUG_LOG, "false");
+        assertThat(Options.isDebugLog(), is(false));
+    }
+
+    @Test
+    public void canSetFreeOption() {
+        Options.setOption("dummy-option", "true");
+        assertThat(Options.is("dummy-option"), is(true));
+    }
+
+    @Test
+    public void canUnsetFreeOption() {
+        Options.setOption("dummy-option", "true");
+        Options.setOption("dummy-option", "false");
+        assertThat(Options.is("dummy-option"), is(false));
+    }
+
+    @Test
+    public void freeOptionDefaultIsFalse() {
+        assertThat(Options.is("dummy-option-2"), is(false));
+    }
+}
