8b685df9f655099d48903e22ff9c4f0b285d9b59
MMatten <mark_matten@hotmail.com>
2015-09-07 21:17:22 +0100

    Install Informix IDS if package available

--- test_vm/vagrant_config_defaults.rb
+++ test_vm/vagrant_config_defaults.rb
@@ -2,6 +2,6 @@ unix_synced_folder_defaults =
   {nfs: true, nfs_version: 4, mount_options: ["noatime", "nodiratime"]}
 
 # Default options
-CONFIG['memory'] = "1100"
+CONFIG['memory'] = "2048"
 CONFIG['synced_folder_options'] = windows? ? nil : unix_synced_folder_defaults
 CONFIG['additional_disk1_file'] = "./disk1.vdi"
