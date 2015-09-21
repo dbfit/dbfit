30994ed3def5c9dfeb72eda7bff4022ebc35ce52
MMatten <mark_matten@hotmail.com>
2015-09-21 18:13:52 +0100

    Apply Java code style - whitespace tidy

--- dbfit-java/informix/src/main/java/dbfit/environment/InformixDateNormalizer.java
+++ dbfit-java/informix/src/main/java/dbfit/environment/InformixDateNormalizer.java
@@ -4,22 +4,17 @@ import dbfit.util.TypeNormaliser;
 
 import java.sql.SQLException;
 
-/**
- * Created by muppana on 7/1/2014.
- */
 public class InformixDateNormalizer implements TypeNormaliser {
 
     @Override
     public Object normalise(Object o) throws SQLException {
         if (o == null)
             return null;
-        if (!(o instanceof  com.informix.jdbc.IfxDate)) {
+        if (!(o instanceof com.informix.jdbc.IfxDate)) {
             throw new UnsupportedOperationException(
-                    "OracleDateNormaliser cannot work with " + o.getClass());
+                    "InformixDataNormalizer cannot work with " + o.getClass());
         }
         com.informix.jdbc.IfxDate ts = ( com.informix.jdbc.IfxDate) o;
         return ts.value();
-
-
     }
 }
