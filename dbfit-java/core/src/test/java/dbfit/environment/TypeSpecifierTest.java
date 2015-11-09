ac48a189e51e01b72ff396911ec0f8be61acedce
MMatten <mark_matten@hotmail.com>
2015-11-09 21:23:22 +0000

    Add basic unit test for TypeSpecifier mechanism

---
+++ dbfit-java/core/src/test/java/dbfit/environment/TypeSpecifierTest.java
@@ -0,0 +1,40 @@
+package dbfit.environment;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import dbfit.api.DBEnvironment;
+
+import dbfit.util.TypeSpecifier;
+
+public class TypeSpecifierTest {
+
+    private class DummyTypeSpecifier implements TypeSpecifier {
+        public Object specify(Object o) throws SQLException {
+            return new String("abcdef");
+        }
+    }
+
+    @Test
+    public void setAndGetTypeSpecifierTest() {
+        DBEnvironment env = dbfit.api.DbEnvironmentFactory.newEnvironmentInstance("Dummy");
+        env.setTypeSpecifier(Integer.class, new DummyTypeSpecifier());
+        TypeSpecifier ts = env.getTypeSpecifier(Integer.class);
+        assertNotNull(ts);
+    }
+
+    @Test
+    public void castTypeTest() {
+        DBEnvironment env = dbfit.api.DbEnvironmentFactory.newEnvironmentInstance("Dummy");
+        env.setTypeSpecifier(java.util.Date.class, new DummyTypeSpecifier());
+        TypeSpecifier ts = env.getTypeSpecifier(java.util.Date.class);
+        String s = null;
+        try {
+            s = (String) ts.specify(new java.util.Date());
+        } catch (SQLException e) {
+            fail();
+        }
+        assertEquals(s, "abcdef");
+    }
+}
