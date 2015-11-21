74b4feede3afe58fbf85b33d942c9861672159d4
MMatten <mark_matten@hotmail.com>
2015-11-21 12:15:43 +0000

    Corrections to exception generation

--- dbfit-java/informix/src/main/java/dbfit/environment/InformixBigDecimalTransformer.java
+++ dbfit-java/informix/src/main/java/dbfit/environment/InformixBigDecimalTransformer.java
@@ -12,7 +12,8 @@ public class InformixBigDecimalTransformer implements TypeTransformer {
             return null;
         }
         if (!(o instanceof dbfit.util.NormalisedBigDecimal)) {
-            throw new UnsupportedOperationException("InformixBigDecimalSpecifier cannot work with " + o.getClass());
+            throw new SQLException(this.getClass().getSimpleName() +
+                    " cannot transform objects of type " + o.getClass().getName());
         }
         java.math.BigDecimal bd = new java.math.BigDecimal(((java.math.BigDecimal) o).toString());
         return bd;
