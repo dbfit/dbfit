bdbd2aa786097e1dc274ba2d177a5507151f198a
Yavor Nikolov <nikolov.javor@gmail.com>
2016-02-14 14:29:41 +0200

    Refactor Update fixture via DbUpdateCommandBuilder

--- dbfit-java/core/src/main/java/dbfit/fixture/Update.java
+++ dbfit-java/core/src/main/java/dbfit/fixture/Update.java
@@ -3,35 +3,28 @@ package dbfit.fixture;
 import dbfit.api.DBEnvironment;
 import dbfit.api.DbEnvironmentFactory;
 import dbfit.api.DbCommand;
-import dbfit.api.DbTable;
+import dbfit.api.DbUpdateCommandBuilder;
 import dbfit.util.DbParameterAccessor;
 import dbfit.util.DbParameterAccessorTypeAdapter;
 import dbfit.util.NameNormaliser;
 import dbfit.util.SymbolAccessSetBinding;
-import dbfit.util.Direction;
 
 import fit.Binding;
 import fit.Parse;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class Update extends fit.Fixture {
     private DBEnvironment environment;
-    private DbCommand statement;
     private String tableName;
     private Binding[] columnBindings;
-    private DbParameterAccessor[] updateAccessors;
-    private DbParameterAccessor[] selectAccessors;
 
     public Update() {
-        this.environment = DbEnvironmentFactory.getDefaultEnvironment();
+        this(DbEnvironmentFactory.getDefaultEnvironment());
     }
 
     public Update(DBEnvironment dbEnvironment) {
-        this.environment = dbEnvironment;
+        this(dbEnvironment, null);
     }
 
     public Update(DBEnvironment dbEnvironment, String tableName) {
@@ -48,15 +41,11 @@ public class Update extends fit.Fixture {
             rows = rows.more;
         }
 
-        try {
-            initParameters(rows.parts); //init parameters from the first row
-            DbTable dbTable = new DbTable(environment, tableName);
-            try (DbCommand satement = dbTable.buildUpdateCommand(selectAccessors, updateAccessors)) {
-                this.statement = statement;
-                Parse row = rows;
-                while ((row = row.more) != null) {
-                    runRow(row);
-                }
+        // init parameters from the first row
+        try (DbCommand statement = buildUpdateCommand(rows.parts)) {
+            Parse row = rows;
+            while ((row = row.more) != null) {
+                runRow(row, statement);
             }
         } catch (Throwable e) {
             e.printStackTrace();
@@ -64,48 +53,49 @@ public class Update extends fit.Fixture {
         }
     }
 
-    private void initParameters(Parse headerCells) throws SQLException {
-        Map<String, DbParameterAccessor> allParams =
-            environment.getAllColumns(tableName);
-        if (allParams.isEmpty()) {
-            throw new SQLException("Cannot retrieve list of columns for " + tableName + " - check spelling and access rights");
-        }
-        columnBindings = new Binding[headerCells.size()];
-        List<DbParameterAccessor> selectAcc = new ArrayList<DbParameterAccessor>();
-        List<DbParameterAccessor> updateAcc = new ArrayList<DbParameterAccessor>();
+    private DbCommand buildUpdateCommand(Parse headerCells) {
+        try {
+            DbUpdateCommandBuilder updateBuilder =
+                new DbUpdateCommandBuilder(environment, tableName);
+            columnBindings = new Binding[headerCells.size()];
 
-        for (int i = 0; headerCells != null; i++, headerCells = headerCells.more) {
-            String name = headerCells.text();
-            String paramName = NameNormaliser.normaliseName(name);
-            //need to clone db param accessors here because same column may be in the update and select part
-            DbParameterAccessor orig = allParams.get(paramName);
-            if (orig == null) {
-                wrong(headerCells);
-                throw new SQLException("Cannot find column " + paramName);
+            for (int i = 0; headerCells != null; i++, headerCells = headerCells.more) {
+                DbParameterAccessor acc = addParameter(headerCells.text(), updateBuilder);
+                initColumnBinding(i, acc);
             }
-            //clone parameter because there may be multiple usages of the same column
-            DbParameterAccessor acc = orig.clone();
-            acc.setDirection(Direction.INPUT);
-            if (headerCells.text().endsWith("=")) {
-                updateAcc.add(acc);
-            } else {
-                selectAcc.add(acc);
-            }
-            columnBindings[i] = new SymbolAccessSetBinding();
-            columnBindings[i].adapter = new DbParameterAccessorTypeAdapter(acc, this);
+
+            return updateBuilder.build();
+        } catch (Exception e) {
+            wrong(headerCells);
+            throw e;
         }
-        // weird jdk syntax, method param is the type of array.
-        selectAccessors = selectAcc.toArray(new DbParameterAccessor[0]);
-        updateAccessors = updateAcc.toArray(new DbParameterAccessor[0]);
     }
 
-    private void runRow(Parse row) throws Throwable {
+    private DbParameterAccessor addParameter(String name, DbUpdateCommandBuilder updateBuilder) {
+        String paramName = NameNormaliser.normaliseName(name);
+
+        if (name.endsWith("=")) {
+            return updateBuilder.addUpdateAccessor(paramName);
+        } else {
+            return updateBuilder.addSelectAccessor(paramName);
+        }
+    }
+
+    private void initColumnBinding(int column, DbParameterAccessor accessor) {
+        columnBindings[column] = new SymbolAccessSetBinding();
+        columnBindings[column].adapter = new DbParameterAccessorTypeAdapter(accessor, this);
+    }
+
+    private void bindRowValues(Parse row) throws Throwable {
+        Parse cell = row.parts;
+        for (int column = 0; column < columnBindings.length; column++, cell = cell.more) {
+            columnBindings[column].doCell(this, cell);
+        }
+    }
+
+    private void runRow(Parse row, DbCommand statement) throws Throwable {
         try {
-            Parse cell = row.parts;
-            //first set input params
-            for (int column = 0; column < columnBindings.length; column++, cell = cell.more) {
-                columnBindings[column].doCell(this, cell);
-            }
+            bindRowValues(row);
             statement.execute();
         } catch (SQLException sqle) {
             sqle.printStackTrace();
