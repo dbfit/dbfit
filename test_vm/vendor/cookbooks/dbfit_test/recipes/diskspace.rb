e919ac8927dad63b8befc95ce6f3d49f7c684401
Mark Matten <mark_matten@hotmail.com>
2016-10-20 12:12:03 +0100

    test_vm: change VB base box to centos/7
    The old one has been moved, is a bit older and and has some
    issues. The new box is effectively 7.2.1511 now.
    
    * change vm.box from chef/centos-7.1
    * update diskspace recipe: since the volume group name is different

--- test_vm/vendor/cookbooks/dbfit_test/recipes/diskspace.rb
+++ test_vm/vendor/cookbooks/dbfit_test/recipes/diskspace.rb
@@ -4,13 +4,13 @@ lvm_physical_volume '/dev/sdb' do
   action :create
 end
 
-lvm_volume_group 'centos' do
+lvm_volume_group 'VolGroup00' do
   action :extend
   physical_volumes ['/dev/sdb']
 end
 
-lvm_logical_volume 'root' do
-  group 'centos'
+lvm_logical_volume 'LogVol00' do
+  group 'VolGroup00'
   action :resize
   size '9G'
   take_up_free_space true
