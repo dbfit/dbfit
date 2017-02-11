1a82cec215425200fe9ce783aaad5dd7c982d8bf
Mark Matten <mark_matten@hotmail.com>
2017-02-11 21:46:08 +0000

    Refactor DB2 integration DB setup

--- dbfit-java/db2/src/integration-test/resources/install_db2.sh
+++ dbfit-java/db2/src/integration-test/resources/install_db2.sh
@@ -144,41 +144,10 @@ then
 	exit 3
 fi
 
-# Set DB2 environment.
-DB2PROFILE=/home/db2inst1/sqllib/db2profile
-
-# Create dftest OS user for dbfit tests.
-echo "$IM creating OS user 'dftest'..."
-useradd dftest
-if [ $? -ne 0 ]
-then
-	echo "$EM creating 'dftest' OS user" 1>&2
-	exit 1
-fi
-
-echo "$IM setting passwd for OS user 'dftest'..."
-echo dftest:DFTEST|chpasswd
-if [ $? -ne 0 ]
-then
-	echo "$EM setting passwd for OS user 'dftest'" 1>&2
-	exit 1
-fi
-
-# Create dftest DB and dftest schema.
-# Run as db2inst1 instance owner.
-echo "$IM creating 'DBFIT' database and 'DFTEST' schema..."
-runuser -l db2inst1 -c "db2 -vf '$DB2_SCRIPTS/create-db-schema-db2.sql'"
+$DB2_SCRIPTS/create_db_schema.sh
 if [ $? -ne 0 ]
 then
 	echo "$EM creating DB2 'DBFIT' DB and 'DFTEST' schema" 1>&2
-	exit 1
-fi
-
-echo "INFO: creating DbFit acceptance testing DB objects..."
-runuser -l dftest -c ". '$DB2PROFILE' && db2 -vf '$DB2_SCRIPTS/acceptancetests-db2.sql'"
-if [ $? -ne 0 ]
-then
-	echo "$EM creating DB2 acceptance testing DB objects" 1>&2
 	exit 1
 fi
 
