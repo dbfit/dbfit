34f532ec6217b51abe851fdff9f48d7b5f6e8128
Yavor Nikolov <nikolov.javor@gmail.com>
2017-03-18 22:55:59 +0200

    Allow specifying parse delegete in SetParameter fixture
    Add an optional 3rd parameter of SetParameter fixture pointing to a
    parse delegate which to parse the given text to Object. Classes with
    static parse(String) or valueOf(String) methods are supported as parse
    delegates. If both valueOf and parse methods are present, valueOf is
    with higher priority.

--- dbfit-java/core/src/main/java/dbfit/util/ValueParser.java
+++ dbfit-java/core/src/main/java/dbfit/util/ValueParser.java
@@ -1,5 +1,8 @@
 package dbfit.util;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
 public class ValueParser {
 
     public Object parse(String text) {
@@ -9,6 +12,30 @@ public class ValueParser {
             return SymbolUtil.getSymbol(text);
         } else {
             return text;
+        }
+    }
+
+    public Object parse(String text, Class<?> parseDelegateClass) {
+        try {
+            return findParsingMethod(parseDelegateClass).invoke(null, text);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public Object parse(String text, String parseDelegateClassName) {
+        try {
+            return parse(text, Class.forName(parseDelegateClassName));
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private Method findParsingMethod(Class<?> cls) throws NoSuchMethodException {
+        try {
+            return cls.getMethod("valueOf", String.class);
+        } catch (NoSuchMethodException e) {
+            return cls.getMethod("parse", String.class);
         }
     }
 }
--- dbfit-java/core/src/test/java/dbfit/util/ValueParserTest.java
+++ dbfit-java/core/src/test/java/dbfit/util/ValueParserTest.java
@@ -1,5 +1,7 @@
 package dbfit.util;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -30,6 +32,23 @@ public class ValueParserTest {
     @Test
     public void canParseMixedCaseNullString() {
         assertEquals(null, parser.parse("NulL"));
+    }
+
+    @Test
+    public void canParseWithParseDelegateClass() {
+        NormalisedBigDecimal expected = new NormalisedBigDecimal(new BigDecimal(123));
+        assertEquals(expected, parser.parse("123", BigDecimalParseDelegate.class));
+    }
+
+    @Test
+    public void canParseWithParseDelegateClassName() {
+        NormalisedBigDecimal expected = new NormalisedBigDecimal(new BigDecimal(123));
+        assertEquals(expected, parser.parse("123", "dbfit.util.BigDecimalParseDelegate"));
+    }
+
+    @Test
+    public void canParseUsingDelgateWithValueOfMethod() {
+        assertEquals(Integer.valueOf(7), parser.parse("7", "java.lang.Integer"));
     }
 
     @Test
