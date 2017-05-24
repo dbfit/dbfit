57536f8f31b602aecc88de45d525f82b57c94923
MMatten <mark_matten@hotmail.com>
2017-05-24 20:38:09 +0100

    Increase VM RAM as SQL Server requires 3.25GB+

--- test_vm/vagrant_config_defaults.rb
+++ test_vm/vagrant_config_defaults.rb
@@ -2,7 +2,7 @@ unix_synced_folder_defaults =
   {nfs: true, nfs_version: 4, nfs_udp: false, mount_options: ["noatime", "nodiratime"]}
 
 # Default options
-CONFIG['memory'] = "1100"
+CONFIG['memory'] = "3350"
 CONFIG['synced_folder_options'] = windows? ? nil : unix_synced_folder_defaults
 CONFIG['additional_disk1_file'] = "./disk1.vdi"
 CONFIG['additional_disk1_size'] = 10 * 1024
