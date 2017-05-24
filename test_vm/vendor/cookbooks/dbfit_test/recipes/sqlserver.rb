2e4b15049c99537a692f355c1885fcc2c4240cfd
MMatten <mark_matten@hotmail.com>
2017-05-24 18:22:45 +0100

    Add SQL Server provisioning recipe

---
+++ test_vm/vendor/cookbooks/dbfit_test/recipes/sqlserver.rb
@@ -0,0 +1,54 @@
+remote_file '/etc/yum.repos.d/mssql-server.repo' do
+  source 'https://packages.microsoft.com/config/rhel/7/mssql-server.repo'
+  action :create
+end
+
+package 'mssql-server'
+
+sa_usr = 'sa'
+sa_pwd = 'DbFit000!'
+sql_host = 'localhost'
+
+ENV['SA_PASSWORD'] = sa_pwd
+ENV['ACCEPT_EULA'] = 'Y'
+
+service 'stop mssql-server' do
+  service_name 'mssql-server'
+  action :stop
+end
+
+execute 'configure SQL Server' do
+  command '/opt/mssql/bin/mssql-conf setup'
+  action :run
+  not_if 'systemctl status mssql-server | grep active (running)'
+end
+
+service 'start mssql-server' do
+  service_name 'mssql-server'
+  action :start
+end
+
+remote_file '/etc/yum.repos.d/prod.repo' do
+  source 'https://packages.microsoft.com/config/rhel/7/prod.repo'
+  action :create
+end
+
+package 'mssql-tools'
+
+sql_dir = File.join(node['dbfit']['project_root'], "test_vm/scripts/sqlserver/sql")
+sql_bin = '/opt/mssql-tools/bin'
+sql_cmd = "#{sql_bin}/sqlcmd -S #{sql_host} -U #{sa_usr} -P #{sa_pwd} -b "
+
+detect_changelog = "#{sql_cmd} -Q 'SELECT COUNT(*) FROM dbfit.dbo.changelog'"
+
+execute 'create databases' do
+  command "#{sql_cmd} -i #{sql_dir}/create-databases-sqlserver.sql"
+  action :run
+  not_if "#{detect_changelog}"
+end
+
+execute 'create dbfit.changelog table' do
+  command "#{sql_cmd} -i #{sql_dir}/create-dbdeploy-changelog-sqlserver.sql"
+  action :run
+  not_if "#{detect_changelog}"
+end
