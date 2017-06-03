ae5d77c417bd5972bad8be8fbe4f634bacb9916b
MMatten <mark_matten@hotmail.com>
2017-06-03 20:31:58 +0100

    Include SQL Server integration tests in Gradle tasks

--- test_vm/vendor/cookbooks/dbfit_test/recipes/gradle.rb
+++ test_vm/vendor/cookbooks/dbfit_test/recipes/gradle.rb
@@ -1,7 +1,7 @@
 project_root = node['dbfit']['project_root']
   
 file "#{project_root}/gradle.properties" do
-  content "excludeIntegrationTests=netezza,sqlserver,teradata\n"
+  content "excludeIntegrationTests=netezza,teradata\n"
   mode '0644'
   owner 'vagrant'
   group 'vagrant'
