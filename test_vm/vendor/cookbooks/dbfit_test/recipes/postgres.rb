549a493eb1348aabff7f6f6bab1317acfecff1c2
MMatten <mark_matten@hotmail.com>
2018-08-01 17:38:09 +0000

    Migrate away from deprecated database cookbook for PostgreSQL

--- test_vm/vendor/cookbooks/dbfit_test/recipes/postgres.rb
+++ test_vm/vendor/cookbooks/dbfit_test/recipes/postgres.rb
@@ -1,41 +1,49 @@
-include_recipe "postgresql::server"
-include_recipe "postgresql::client"
-include_recipe "postgresql::ruby"
+postgresql_host = "127.0.0.1"
+postgresql_port = 5432
+postgresql_username = 'postgres'
+postgresql_password = node['postgresql']['password']['postgres']
 
-include_recipe "database::postgresql"
-
-postgresql_connection_info = {:host => "127.0.0.1",
-                              :port => 5432,
-                              :username => 'postgres',
-                              :password => node['postgresql']['password']['postgres']}
+test_database = 'dbfit'
+test_username = 'dbfit'
+test_password = 'dbfit'
 
 sql_dir = File.join(node['dbfit']['project_root'], "test_vm/scripts/postgresql/sql")
 
-postgresql_database 'dbfit' do
-  connection postgresql_connection_info
+postgresql_server_install 'Install Postgresql server' do
+  action :install
+end
+
+postgresql_server_install 'Setup Postgresql server' do
+  password postgresql_password
+  port postgresql_port
   action :create
 end
 
-postgresql_database_user 'create dbfit user' do
-  connection postgresql_connection_info
-  username 'dbfit'
-  password 'dbfit'
+postgresql_client_install 'Setup Postgresql client' do
+  action :install
+end
+
+postgresql_database test_database do
+  host postgresql_host
+  port postgresql_port
+  user postgresql_username
   action :create
 end
 
-postgresql_database_user 'dbfit grants' do
-  connection postgresql_connection_info
-  username 'dbfit'
-  database_name 'dbfit'
-  privileges [:all]
+postgresql_user test_username do
+  password test_password
+  superuser true
+  action :create
+end
+
+postgresql_access 'dbfit grants' do
+  access_type 'local'
+  access_user test_username
+  access_db test_database
+  access_method 'md5'
   action :grant
 end
 
-# needed to support DbDeploy
-postgresql_database 'dbfit.changelog' do
-  connection postgresql_connection_info
-  database_name 'dbfit'
-  sql { ::File.open("#{sql_dir}/create-db-schema-postgresql.sql").read }
-  action :query
+execute "Create dbdeploy changelog table" do
+    command "psql -U #{postgresql_username} -d #{test_database} -f #{sql_dir}/create-db-schema-postgresql.sql"
 end
-
