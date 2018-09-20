fe34b0436f8d83430a249aa20198bf5ea2f29215
MMatten <mark_matten@hotmail.com>
2018-09-20 21:39:11 +0000

    Getting ASE tests running

--- test_vm/vendor/cookbooks/dbfit_test/recipes/sybase.rb
+++ test_vm/vendor/cookbooks/dbfit_test/recipes/sybase.rb
@@ -10,3 +10,12 @@ execute 'install Sybase IQ package if available' do
   returns [0, 2, 3]
 end
 
+execute 'install Sybase ASE package if available' do
+  project_root = node['dbfit']['project_root']
+
+  user 'root'
+  command "#{project_root}/test_vm/scripts/sybase/install_sybasease.sh -dl"
+  not_if "#{project_root}/test_vm/scripts/sybase/detect_sybasease.sh"
+  action :run
+  returns [0, 2, 3]
+end
