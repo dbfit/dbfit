3bab53e1b6dc6cde14191476410c097b79a6a5b9
Mark Matten <mark_matten@hotmail.com>
2018-10-20 17:18:41 +0100

    Normalise java.sql.Timestamp by instantiation instead of casting to support Sybase ASE

---
+++ dbfit-java/core/src/main/java/dbfit/util/SqlTimestampNormaliser.java
@@ -0,0 +1,22 @@
+package dbfit.util;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import dbfit.util.TypeTransformer;
+
+public class SqlTimestampNormaliser implements TypeTransformer {
+
+	@Override
+    public Object transform(Object o) throws SQLException {
+        if (o == null) {
+            return null;
+        }
+        if (!(o instanceof Timestamp)) {
+            throw new UnsupportedOperationException(
+                    "SqlTimestampNormaliser cannot work with "
+                            + o.getClass());
+        }
+        return Timestamp.valueOf(o.toString());
+    }
+}
