d79fbbe3c0461785556ec3047a9902c34ca0c883
Bodo Hüsemann <bodo.huesemann@gmx.de>
2020-08-04 23:30:03 +0200

    fix oracle deprecations with jdk8

--- dbfit-java/oracle/src/main/java/dbfit/environment/OracleClobNormaliser.java
+++ dbfit-java/oracle/src/main/java/dbfit/environment/OracleClobNormaliser.java
@@ -12,17 +12,14 @@ public class OracleClobNormaliser implements TypeTransformer {
     public Object transform(Object o) throws SQLException {
         if (o == null)
             return null;
-        if (!(o instanceof oracle.sql.CLOB)) {
-            throw new UnsupportedOperationException(
-                    "OracleClobNormaliser cannot work with " + o.getClass());
+        if (!(o instanceof java.sql.Clob)) {
+            throw new UnsupportedOperationException("OracleClobNormaliser cannot work with " + o.getClass());
         }
-        oracle.sql.CLOB clob = (oracle.sql.CLOB) o;
+        java.sql.Clob clob = (java.sql.Clob) o;
         if (clob.length() > MAX_CLOB_LENGTH)
-            throw new UnsupportedOperationException("Clobs larger than "
-                    + MAX_CLOB_LENGTH + " bytes are not supported by DBFIT");
-        char[] buffer = new char[MAX_CLOB_LENGTH];
-        int total = clob.getChars(1, MAX_CLOB_LENGTH, buffer);
-        return String.valueOf(buffer, 0, total);
+            throw new UnsupportedOperationException(
+                    "Clobs larger than " + MAX_CLOB_LENGTH + " bytes are not supported by DBFIT");
+        return clob.getSubString(1, MAX_CLOB_LENGTH);
     }
 
 }
